81321fd98f7b5d97481bbb919eaaaf684f8516f8
henriyli <henri.ylikotila@helsinki.fi>
2014-07-08 16:19:38 +0300

    the no results label needs to be removed when the search is performed again

--- resource/js/docready.js
+++ resource/js/docready.js
@@ -837,6 +837,7 @@ $(function() { // DOCUMENT READY
   }
 
   function loadLimitations() {
+    $('#alphabetical-menu').empty();
     var $loading = $("<div class='search-result'><p>" + loading_text + "&hellip;<span class='spinner'/></p></div>"); 
     $('.search-result-listing').empty();
     $('.search-result-listing').append($loading);
