3a6a83c05b02f48e4673ef4a124588a8fb8ca072
henriyli <henri.ylikotila@helsinki.fi>
2014-08-19 13:00:54 +0300

    fixed the broken apc-caching in the getResourceFromUri related to #39

--- model/Model.php
+++ model/Model.php
@@ -432,19 +432,19 @@ class Model
       // using apc cache for the resource if available
       if (function_exists('apc_store') && function_exists('apc_fetch')) {
         $key = 'fetch: ' . $uri;
-        $this->resource = apc_fetch($key);
-        if ($this->resource === null || $this->resource === FALSE) { // was not found in cache
+        $resource = apc_fetch($key);
+        if ($resource === null || $resource === FALSE) { // was not found in cache
           $client = EasyRdf_Graph::newAndLoad($uri);
-          $this->resource = $client->resource($uri);
+          $resource = $client->resource($uri);
           apc_store($key, $resource);
         }
       } else { // APC not available, parse on every request
         $client = EasyRdf_Graph::newAndLoad($uri);
-        $this->resource = $client->resource($uri);
+        $resource = $client->resource($uri);
       }
     } catch (Exception $e) {
     }
-    return $this->resource;
+    return $resource;
   }
 
   /**
