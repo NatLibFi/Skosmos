7bff0f1f2b103174780cf4e77b84934f49574f38
henriyli <henri.ylikotila@helsinki.fi>
2014-09-05 13:31:40 +0300

    more tests

--- tests/VocabularyTest.php
+++ tests/VocabularyTest.php
@@ -38,5 +38,24 @@ class VocabularyTest extends PHPUnit_Framework_TestCase
     $langs = $vocab->getLanguages();
     $this->assertEquals(2, sizeof($langs));
   }
+  
+  /**
+   * @covers Vocabulary::getDefaultLanguage
+   */
+  public function testGetDefaultLanguage() {
+    $vocab = $this->model->getVocabulary('test');
+    $lang = $vocab->getDefaultLanguage();
+    $this->assertEquals('en', $lang);
+  }
+  
+  /**
+   * @covers Vocabulary::getDefaultLanguage
+   * @expectedException \Exception
+   * @expectedExceptionMessage Default language for vocabulary 'testdiff' unknown, choosing
+   */
+  public function testGetDefaultLanguageWhenNotSet() {
+    $vocab = $this->model->getVocabulary('testdiff');
+    $lang = $vocab->getDefaultLanguage();
+  }
 
 }
