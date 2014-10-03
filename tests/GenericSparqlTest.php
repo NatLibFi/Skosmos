b592cee3c02236a001316e76ad0b30ae8f6e42b9
henriyli <henri.ylikotila@helsinki.fi>
2014-10-03 13:35:00 +0300

    wrote a test for issue #103

--- tests/GenericSparqlTest.php
+++ tests/GenericSparqlTest.php
@@ -549,26 +549,28 @@ class GenericSparqlTest extends PHPUnit_Framework_TestCase
 
   /**
    * @covers GenericSparql::listConceptGroups
-   * @todo   Implement testListConceptGroups().
    */
   public function testListConceptGroups()
   {
-    // Remove the following lines when you implement this test.
-    $this->markTestIncomplete(
-      'This test has not been implemented yet.'
-    );
-  }
+    $voc = $this->model->getVocabulary('groups');
+    $graph = $voc->getGraph();
+    $sparql = new GenericSparql('http://localhost:3030/ds/sparql', $graph, $this->model);
+    $actual = $sparql->ListConceptGroups('http://www.w3.org/2004/02/skos/core#Collection', 'en');
+    $this->assertEquals('Freshwater fish', $actual['http://www.skosmos.skos/groups/fresh']);
+    $this->assertEquals('Saltwater fish', $actual['http://www.skosmos.skos/groups/salt']);
+}
 
   /**
    * @covers GenericSparql::listConceptGroupContents
-   * @todo   Implement testListConceptGroupContents().
    */
-  public function testListConceptGroupContents()
+  public function testListConceptGroupContentsIncludingDeprecatedConcept()
   {
-    // Remove the following lines when you implement this test.
-    $this->markTestIncomplete(
-      'This test has not been implemented yet.'
-    );
+    $voc = $this->model->getVocabulary('groups');
+    $graph = $voc->getGraph();
+    $sparql = new GenericSparql('http://localhost:3030/ds/sparql', $graph, $this->model);
+    $actual = $sparql->ListConceptGroupContents('http://www.w3.org/2004/02/skos/core#Collection', 'http://www.skosmos.skos/groups/salt', 'en');
+    $this->assertArrayHasKey('http://www.skosmos.skos/groups/ta113', $actual);
+    $this->assertArrayNotHasKey('http://www.skosmos.skos/groups/ta111', $actual);
   }
 }
   
