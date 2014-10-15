978cc692cfba8b37e1cc49dfec8725e795db8b83
henriyli <henri.ylikotila@helsinki.fi>
2014-10-15 10:10:05 +0300

    updated the fuseki dist version number

--- tests/init_fuseki.sh
+++ tests/init_fuseki.sh
@@ -1,10 +1,10 @@
-if [ ! -f jena-fuseki-1.1.0/fuseki-server ]; then
+if [ ! -f jena-fuseki-1.1.1/fuseki-server ]; then
   echo "fuseki server file not found - downloading it"
-  wget --output-document=fuseki-dist.tar.gz http://www.nic.funet.fi/pub/mirrors/apache.org//jena/binaries/jena-fuseki-1.1.0-distribution.tar.gz
+  wget --output-document=fuseki-dist.tar.gz http://www.nic.funet.fi/pub/mirrors/apache.org//jena/binaries/jena-fuseki-1.1.1-distribution.tar.gz
   tar -zxvf fuseki-dist.tar.gz
 fi
 
-cd jena-fuseki-1.1.0
+cd jena-fuseki-1.1.1
 ./fuseki-server --config ../fuseki-assembler.ttl &
 until $(curl --output /dev/null --silent --head --fail http://localhost:3030); do
   printf '.'
