9e23456836c09d6b4aeb30485b953c2e126cd473
henriyli <henri.ylikotila@helsinki.fi>
2015-03-19 15:16:50 +0200

    cleaning up ConceptMappingPropertyValue

--- model/ConceptMappingPropertyValue.php
+++ model/ConceptMappingPropertyValue.php
@@ -5,37 +5,14 @@
  */
 class ConceptMappingPropertyValue extends VocabularyDataObject
 {
-  /** language code of the value literal */
-  private $lang;
-  /** if the concept is inherited from a another vocabulary store that identifier here */
-  private $exvocab;
   /** property type */
   private $type;
-  /** literal value of the property */
-  private $label;
-  /** uri of the concept the property value belongs to */
-  private $uri;
-  /** id of the vocabulary the concept belongs to */
-  protected $vocab;
-  /** vocabulary label */
-  private $vocabName;
-  /** if the property is a subProperty of a another property */
-  private $parentProperty;
-  private $submembers;
 
-  //public function __construct($prop, $uri, $vocab, $lang, $label, $exvocab = null, $parent = null, $vocabname = null)
   public function __construct($model, $vocab, $resource, $prop)
   {
     parent::__construct($model, $vocab, $resource);
     $this->submembers = array();
-    //$this->lang = $lang;
-    //$this->exvocab = $exvocab;
     $this->type = $prop;
-    //$this->label = $label;
-    //$this->uri = $uri;
-    //$this->vocab = $vocab;
-    //$this->vocabName = $vocabname;
-    //$this->parentProperty = $parent;
   }
 
   public function __toString()
@@ -43,16 +20,6 @@ class ConceptMappingPropertyValue extends VocabularyDataObject
     if ($this->label === null)
       return "";
     return $this->label;
-  }
-
-  public function getLang()
-  {
-    return $this->lang;
-  }
-
-  public function getExVocab()
-  {
-    return $this->exvocab;
   }
 
   public function getType()
@@ -88,11 +55,6 @@ class ConceptMappingPropertyValue extends VocabularyDataObject
   public function getUri()
   {
     return $this->resource->getUri();
-  }
-
-  public function getParent()
-  {
-    return $this->parentProperty;
   }
 
   public function getVocab()
