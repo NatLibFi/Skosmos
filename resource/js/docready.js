e712c10c30afa6f169567cce243475a0f647605b
henriyli <henri.ylikotila@helsinki.fi>
2015-05-19 09:34:56 +0300

    moving the ajax queue to the scripts file

--- resource/js/docready.js
+++ resource/js/docready.js
@@ -4,34 +4,6 @@
  * see LICENSE.txt for more information
  */
 
- /*
-  * Ajax query queue that keeps track of ongoing queries 
-  * so they can be cancelled if a another event is triggered.
-  */
-$.ajaxQ = (function(){
-  var id = 0, Q = {};
-
-  $(document).ajaxSend(function(e, jqx){
-    jqx._id = ++id;
-    Q[jqx._id] = jqx;
-  });
-  $(document).ajaxComplete(function(e, jqx){
-    delete Q[jqx._id];
-  });
-
-  return {
-    abortAll: function(){
-      var r = [];
-      $.each(Q, function(i, jqx){
-        r.push(jqx._id);
-        jqx.abort();
-      });
-      return r;
-    }
-  };
-
-})();
-
 $(function() { // DOCUMENT READY 
 
   var spinner = '<div class="loading-spinner"><span class="spinner-text">'+ loading_text + '</span><span class="spinner" /></div>';
--- resource/js/scripts.js
+++ resource/js/scripts.js
@@ -59,3 +59,31 @@ function debounce(func, wait, immediate) {
 		if (callNow) func.apply(context, args);
 	};
 }
+
+ /*
+  * Ajax query queue that keeps track of ongoing queries 
+  * so they can be cancelled if a another event is triggered.
+  */
+$.ajaxQ = (function(){
+  var id = 0, Q = {};
+
+  $(document).ajaxSend(function(e, jqx){
+    jqx._id = ++id;
+    Q[jqx._id] = jqx;
+  });
+  $(document).ajaxComplete(function(e, jqx){
+    delete Q[jqx._id];
+  });
+
+  return {
+    abortAll: function(){
+      var r = [];
+      $.each(Q, function(i, jqx){
+        r.push(jqx._id);
+        jqx.abort();
+      });
+      return r;
+    }
+  };
+
+})();
