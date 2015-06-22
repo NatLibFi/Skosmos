e47beaa4f36d4a9c015c74f76a0bb56d89c63149
henriyli <henri.ylikotila@helsinki.fi>
2015-06-22 15:11:30 +0300

    removing redundant require_once for Request

--- model/Model.php
+++ model/Model.php
@@ -23,7 +23,6 @@ require_once 'model/ConceptPropertyValueLiteral.php';
 require_once 'model/VocabularyCategory.php';
 require_once 'model/Vocabulary.php';
 require_once 'model/Breadcrumb.php';
-require_once 'model/Request.php';
 
 require_once 'model/sparql/GenericSparql.php';
 require_once 'model/sparql/JenaTextSparql.php';
