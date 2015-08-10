9e6499c9b8b36024852576e72788ca3fe48af39c
henriyli <henri.ylikotila@helsinki.fi>
2015-08-10 14:36:32 +0300

    added a test for ConceptProperty

--- tests/ConceptPropertyTest.php
+++ tests/ConceptPropertyTest.php
@@ -45,4 +45,18 @@ class ConceptPropertyTest extends PHPUnit_Framework_TestCase
     $props = $concept->getProperties();
     $this->assertEquals('skos:definition', $props['skos:definition']->getType());
   }
+
+  /**
+   * @covers ConceptProperty::addValue
+   * @covers ConceptProperty::sortValues
+   */
+  public function testAddValue() {
+    $vocab = $this->model->getVocabulary('test');
+    $concepts = $vocab->getConceptInfo('http://www.skosmos.skos/test/ta1', 'en');
+    $concept = $concepts[0];
+    $props = $concept->getProperties();
+    $prop = $props['skos:narrower']->getValues();
+    $expected_order = array (0 => '3D Basshttp://www.skosmos.skos/test/ta117',1 => 'Basshttp://www.skosmos.skos/test/ta116',2 => 'Burihttp://www.skosmos.skos/test/ta114',3 => 'Carphttp://www.skosmos.skos/test/ta112',4 => 'Eelhttp://www.skosmos.skos/test/ta115',5 => 'Haukihttp://www.skosmos.skos/test/ta119',6 => 'Tunahttp://www.skosmos.skos/test/ta111',7 => 'test:ta113http://www.skosmos.skos/test/ta113',8 => 'test:ta120http://www.skosmos.skos/test/ta120');
+    $this->assertEquals($expected_order, array_keys($prop));
+  }
 }
