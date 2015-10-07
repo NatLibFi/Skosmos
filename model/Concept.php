781c9b1348b42aa25221a884f0344f51c87c34e5
Osma Suominen <osma.suominen@helsinki.fi>
2015-10-07 14:01:56 +0300

    make comparison unambiguous #3

--- model/Concept.php
+++ model/Concept.php
@@ -286,7 +286,7 @@ class Concept extends VocabularyDataObject
     }
 
     foreach ($long_uris as &$prop) {
-      if (EasyRdf_Namespace::shorten($prop)) // shortening property labels if possible
+      if (EasyRdf_Namespace::shorten($prop) !== null) // shortening property labels if possible
         $prop = $sprop = EasyRdf_Namespace::shorten($prop);
       else
         $sprop = "<$prop>"; // EasyRdf requires full URIs to be in angle brackets
