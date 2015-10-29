160906cdda85432f533413efc85f3df7e22f91d4
Osma Suominen <osma.suominen@helsinki.fi>
2015-10-29 13:23:37 +0200

    remove traceback code as it may cause security issues

--- model/DataObject.php
+++ model/DataObject.php
@@ -125,13 +125,8 @@ class DataObject
 
             return $this->lang;
         }
-        $trace = debug_backtrace();
-        trigger_error(
-            'Undefined property via __get(): ' . $name .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_NOTICE);
 
+        trigger_error("Undefined property accessed via __get(): $name", E_USER_NOTICE);
         return null;
     }
 
