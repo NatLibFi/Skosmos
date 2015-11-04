93ed7b8e38c7906d2e189dc5a955adf8f66cb9b0
Osma Suominen <osma.suominen@aalto.fi>
2015-11-04 13:13:56 +0200

    switch tests to use Fuseki 1.3.0 (Jena3) with the new text index, with Travis 12.04 environment and installing JDK 8

--- tests/init_fuseki.sh
+++ tests/init_fuseki.sh
@@ -1,10 +1,16 @@
-if [ ! -f jena-fuseki1-1.1.2/fuseki-server ]; then
+# see what JVMs are available
+ls -l /usr/lib/jvm/
+
+# try using Oracle JDK 8
+jdk_switcher use oraclejdk8
+
+if [ ! -f jena-fuseki1-1.3.0/fuseki-server ]; then
   echo "fuseki server file not found - downloading it"
-  wget --output-document=fuseki-dist.tar.gz http://archive.apache.org/dist/jena/binaries/jena-fuseki1-1.1.2-distribution.tar.gz 
+  wget --output-document=fuseki-dist.tar.gz http://archive.apache.org/dist/jena/binaries/jena-fuseki1-1.3.0-distribution.tar.gz 
   tar -zxvf fuseki-dist.tar.gz
 fi
 
-cd jena-fuseki1-1.1.2
+cd jena-fuseki1-1.3.0
 ./fuseki-server --config ../fuseki-assembler.ttl &
 until $(curl --output /dev/null --silent --head --fail http://localhost:3030); do
   printf '.'
