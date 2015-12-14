8c9f25d7b903c7e3895ad5ef28abe593fc79a2e5
henriyli <henri.ylikotila@helsinki.fi>
2015-12-14 12:45:32 +0200

    more default value checks for GlobalConfig

--- tests/GlobalConfigTest.php
+++ tests/GlobalConfigTest.php
@@ -66,5 +66,77 @@ class GlobalConfigTest extends PHPUnit_Framework_TestCase
     $actual = $this->config->getTemplateCache();
     $this->assertEquals('/tmp/skosmos-template-cache', $actual);
   }
+
+  /**
+   * @covers GlobalConfig::getDefaultSparqlDialect
+   */
+  public function testSparqlDialectDefaultValue() {
+    $actual = $this->config->getDefaultSparqlDialect();
+    $this->assertEquals('Generic', $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getFeedbackAddress
+   */
+  public function testFeedbackAddressDefaultValue() {
+    $actual = $this->config->getFeedbackAddress();
+    $this->assertEquals(null, $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getLogCaughtExceptions
+   */
+  public function testExceptionLoggingDefaultValue() {
+    $actual = $this->config->getLogCaughtExceptions();
+    $this->assertEquals(null, $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getServiceName
+   */
+  public function testServiceNameDefaultValue() {
+    $actual = $this->config->getServiceName();
+    $this->assertEquals('Skosmos', $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getServiceTagline
+   */
+  public function testgetServiceTaglineDefaultValue() {
+    $actual = $this->config->getServiceTagline();
+    $this->assertEquals(null, $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getServiceLogo
+   */
+  public function testServiceLogoDefaultValue() {
+    $actual = $this->config->getServiceLogo();
+    $this->assertEquals(null, $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getCustomCss
+   */
+  public function testCustomCssDefaultValue() {
+    $actual = $this->config->getCustomCss();
+    $this->assertEquals(null, $actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getUILanguageDropdown
+   */
+  public function testDefaultValue() {
+    $actual = $this->config->getUILanguageDropdown();
+    $this->assertFalse($actual);
+  }
+
+  /**
+   * @covers GlobalConfig::getBaseHref
+   */
+  public function testBaseHrefDefaultValue() {
+    $actual = $this->config->getBaseHref();
+    $this->assertEquals(null, $actual);
+  }
 }
 
