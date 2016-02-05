c404ca6b35a5144ed075c32f5eeb2aba6348d7c6
henriyli <henri.ylikotila@helsinki.fi>
2016-02-05 10:49:03 +0200

    adding tests for JenaTextSparql

---
+++ tests/JenaTextSparqlTest.php
@@ -0,0 +1,163 @@
+<?php
+
+class JenaTextSparqlTest extends PHPUnit_Framework_TestCase
+{
+  private $model; 
+  private $graph; 
+  private $sparql;
+  private $vocab;
+  private $params;
+
+  protected function setUp() {
+    putenv("LC_ALL=en_GB.utf8");
+    setlocale(LC_ALL, 'en_GB.utf8');
+    $this->model = new Model(new GlobalConfig('/../tests/jenatestconfig.inc'));
+    $this->vocab = $this->model->getVocabulary('test');
+    $this->graph = $this->vocab->getGraph();
+    $this->params = $this->getMockBuilder('ConceptSearchParameters')->disableOriginalConstructor()->getMock();
+    $this->sparql = new JenaTextSparql('http://localhost:3030/ds/sparql', $this->graph, $this->model);
+  }
+ 
+  /**
+   * @covers JenaTextSparql::__construct
+   */
+  public function testConstructor() {
+    $gs = new JenaTextSparql('http://localhost:3030/ds/sparql', $this->graph, $this->model);
+    $this->assertInstanceOf('JenaTextSparql', $gs);
+  }
+  
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabetical() {
+    $actual = $this->sparql->queryConceptsAlphabetical('b', 'en');
+    $expected = array (
+      0 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta116',
+        'localname' => 'ta116',
+        'prefLabel' => 'Bass',
+        'lang' => 'en',
+      ),
+      1 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta122',
+        'localname' => 'ta122',
+        'prefLabel' => 'Black sea bass',
+        'lang' => 'en',
+      ),
+      2 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta114',
+        'localname' => 'ta114',
+        'prefLabel' => 'Buri',
+        'lang' => 'en',
+      ),
+    );
+    $this->assertEquals($expected, $actual);
+  }
+  
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabeticalLimit() {
+    $actual = $this->sparql->queryConceptsAlphabetical('b', 'en', 2);
+    $expected = array (
+      0 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta116',
+        'localname' => 'ta116',
+        'prefLabel' => 'Bass',
+        'lang' => 'en',
+      ),
+      1 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta122',
+        'localname' => 'ta122',
+        'prefLabel' => 'Black sea bass',
+        'lang' => 'en',
+      ),
+    );
+    $this->assertEquals($expected, $actual);
+  }
+  
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabeticalLimitAndOffset() {
+    $actual = $this->sparql->queryConceptsAlphabetical('b', 'en', 2, 1);
+    $expected = array (
+      0 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta122',
+        'localname' => 'ta122',
+        'prefLabel' => 'Black sea bass',
+        'lang' => 'en',
+      ),
+      1 => array (
+        'uri' => 'http://www.skosmos.skos/test/ta114',
+        'localname' => 'ta114',
+        'prefLabel' => 'Buri',
+        'lang' => 'en',
+      ),
+    );
+    $this->assertEquals($expected, $actual);
+  }
+  
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabeticalNoResults() {
+    $actual = $this->sparql->queryConceptsAlphabetical('x', 'en');
+    $expected = array();
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabeticalSpecialChars() {
+    $actual = $this->sparql->queryConceptsAlphabetical('!*', 'en');
+    $this->assertEquals(1, sizeof($actual));
+    $this->assertEquals('-"special" character \\example\\', $actual[0]['prefLabel']);
+  }
+  
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabeticalNumbers() {
+    $actual = $this->sparql->queryConceptsAlphabetical('0-9', 'en');
+    $this->assertEquals(1, sizeof($actual));
+    $this->assertContains("3D", $actual[0]['prefLabel']);
+  }
+  
+  /**
+   * @covers JenaTextSparql::generateAlphabeticalListQuery
+   */
+  public function testQueryConceptsAlphabeticalFull() {
+    $actual = $this->sparql->queryConceptsAlphabetical('*', 'en');
+    $this->assertEquals(11, sizeof($actual));
+  }
+
+  /**
+   * @covers JenaTextSparql::createTextQueryCondition
+   * @covers JenaTextSparql::generateConceptSearchQueryCondition
+   */
+  public function testQueryConcepts()
+  {
+    $voc = $this->model->getVocabulary('test');
+    $this->params->method('getSearchTerm')->will($this->returnValue('bass*'));
+    $this->params->method('getVocabIds')->will($this->returnValue(array('test')));
+    $actual = $this->sparql->queryConcepts(array($voc), null, null, $this->params);
+    $this->assertEquals(1, sizeof($actual));
+    $this->assertEquals('Bass', $actual[0]['prefLabel']);
+  }
+  
+  /**
+   * @covers JenaTextSparql::createTextQueryCondition
+   * @covers JenaTextSparql::generateConceptSearchQueryCondition
+   */
+  public function testQueryConceptsAsteriskBeforeTerm()
+  {
+    $voc = $this->model->getVocabulary('test');
+    $this->params->method('getSearchTerm')->will($this->returnValue('*bass'));
+    $actual = $this->sparql->queryConcepts(array($voc), null, null, $this->params);
+    $this->assertEquals(3, sizeof($actual));
+    foreach($actual as $match)
+      $this->assertContains('bass', $match['prefLabel'], '',true);
+  }
+}
