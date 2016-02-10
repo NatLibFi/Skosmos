9a2876941c3ce5341b0cc77a958b434f84c5ee47
henriyli <henri.ylikotila@helsinki.fi>
2016-02-10 15:55:26 +0200

    simple and super meaningful test for VocabularyDataObject

---
+++ tests/VocabularyDataObjectTest.php
@@ -0,0 +1,19 @@
+
+<?php
+
+class VocabularyDataObjectTest extends PHPUnit_Framework_TestCase
+{
+
+  /**
+   * @covers VocabularyDataObject::__construct
+   */
+  public function testConstructorNoArguments()
+  {
+    $mockmod = $this->getMockBuilder('Model')->disableOriginalConstructor()->getMock();
+    $mockvoc = $this->getMockBuilder('Vocabulary')->disableOriginalConstructor()->getMock();
+    $mockres = $this->getMockBuilder('EasyRdf_Resource')->disableOriginalConstructor()->getMock();
+    $vocdao = new VocabularyDataObject($mockmod, $mockvoc, $mockres); 
+    $this->assertInstanceOf('VocabularyDataObject', $vocdao);
+  }
+  
+}
