0ca5e6baeae388f22f09b2332ab17813c8710963
henriyli <henri.ylikotila@helsinki.fi>
2016-02-10 13:08:36 +0200

    adding a few tests to VocabularyCategory

---
+++ tests/VocabularyCategoryTest.php
@@ -0,0 +1,65 @@
+<?php
+
+class VocabularyCategoryTest extends PHPUnit_Framework_TestCase
+{
+  private $model; 
+  private $mockres;
+
+  protected function setUp() {
+    require_once 'testconfig.inc';
+    putenv("LC_ALL=en_GB.utf8");
+    setlocale(LC_ALL, 'en_GB.utf8');
+    //bindtextdomain('skosmos', 'resource/translations');
+    //bind_textdomain_codeset('skosmos', 'UTF-8');
+    //textdomain('skosmos');
+
+    $this->model = new Model(new GlobalConfig('/../tests/testconfig.inc'));
+    $this->mockres = $this->getMockBuilder('EasyRdf_Resource')->disableOriginalConstructor()->getMock();
+    $this->mockres->method('localName')->will($this->returnValue('local name'));
+  }
+  
+  /**
+   * @covers VocabularyCategoryTest::__construct
+   * @expectedException Exception
+   * @expectedExceptionMessage Invalid constructor parameter given to DataObject. 
+   */
+  public function testConstructorWithInvalidParameters() {
+    new VocabularyCategory('invalid', 'invalid');
+  }
+  
+  /**
+   * @covers VocabularyCategoryTest::__construct
+   */
+  public function testConstructor() {
+    $cat = new VocabularyCategory($this->model, $this->mockres);
+    $this->assertInstanceOf('VocabularyCategory', $cat);
+  }
+  
+  /**
+   * @covers VocabularyCategoryTest::getVocabularies
+   */
+  public function testGetVocabularies() {
+    $cat = new VocabularyCategory($this->model, null);
+    $vocs = $cat->getVocabularies();
+    foreach ($vocs as $voc) {
+      $this->assertInstanceOf('Vocabulary', $voc);
+    }
+  }
+  
+  /**
+   * @covers VocabularyCategoryTest::getTitle
+   */
+  public function testGetTitle() {
+    $cat = new VocabularyCategory($this->model, $this->mockres);
+    $this->assertEquals('local name', $cat->getTitle());
+  }
+  
+  /**
+   * @covers VocabularyCategoryTest::getTitle
+   */
+  public function testGetTitleWhenNoResourceGiven() {
+    $cat = new VocabularyCategory($this->model, null);
+    $this->assertEquals('Vocabularies', $cat->getTitle());
+  }
+
+}
