0642bccdc359e846bef4daaca4b69ea5d06b9033
henriyli <henri.ylikotila@helsinki.fi>
2016-03-29 11:59:10 +0300

    fixing the changes tab jumping around in the sidebar

--- resource/js/docready.js
+++ resource/js/docready.js
@@ -315,7 +315,6 @@ $(function() { // DOCUMENT READY
             url : targetUrl,
             success : function(data) {
               updateSidebar(data);
-              $('#alpha').after($('#hierarchy'));
               $('.nav').scrollTop(0);
               if (window.history.pushState) { window.history.pushState(null, null, encodeURI(event.target.href)); }
               updateTitle(data);
@@ -339,7 +338,6 @@ $(function() { // DOCUMENT READY
             url : targetUrl,
             success : function(data) {
               updateSidebar(data);
-              $('#changes').after($('#hierarchy'));
               $('.nav').scrollTop(0);
               if (window.history.pushState) { window.history.pushState(null, null, encodeURI(event.target.href)); }
               updateTitle(data);
