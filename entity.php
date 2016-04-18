f5657fbf262b4c14c53fc902cd98f0d0e9c62d2a
Osma Suominen <osma.suominen@helsinki.fi>
2016-04-18 10:25:49 +0300

    add entity.php which was missing. Fixes #359

---
+++ entity.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright (c) 2012-2016 Aalto University and University of Helsinki
+ * MIT License
+ * see LICENSE.txt for more information
+ */
+
+/**
+ * Includes the side wide settings.
+ */
+require_once 'vendor/autoload.php';
+
+header("Access-Control-Allow-Origin: *"); // enable CORS
+
+try {
+    $config = new GlobalConfig();
+    $model = new Model($config);
+    $controller = new EntityController($model);
+    $request = new Request($model);
+    
+    
+    $controller->redirect($request);
+
+} catch (Exception $e) {
+    header("HTTP/1.0 500 Internal Server Error");
+    echo('ERROR: ' . $e->getMessage());
+}
