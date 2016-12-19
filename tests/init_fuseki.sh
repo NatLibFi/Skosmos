226520179d1dc94419dc040afbc246f544571ed8
Osma Suominen <osma.suominen@helsinki.fi>
2016-12-19 17:31:29 +0200

    Run tests using both Fuseki 1.4.0 and 1.4.1, with 1.4.1 allowed to fail. Part of #564

--- tests/init_fuseki.sh
+++ tests/init_fuseki.sh
@@ -1,16 +1,14 @@
-# see what JVMs are available
-ls -l /usr/lib/jvm/
+# Note: This script must be sourced from within bash, e.g. ". init_fuseki.sh"
 
-# try using Oracle JDK 8
-jdk_switcher use oraclejdk8
+FUSEKI_VERSION=${FUSEKI_VERSION:-1.3.0}
 
-if [ ! -f jena-fuseki1-1.3.0/fuseki-server ]; then
+if [ ! -f jena-fuseki1-$FUSEKI_VERSION/fuseki-server ]; then
   echo "fuseki server file not found - downloading it"
-  wget --output-document=fuseki-dist.tar.gz http://archive.apache.org/dist/jena/binaries/jena-fuseki1-1.3.0-distribution.tar.gz 
+  wget --output-document=fuseki-dist.tar.gz http://archive.apache.org/dist/jena/binaries/jena-fuseki1-$FUSEKI_VERSION-distribution.tar.gz 
   tar -zxvf fuseki-dist.tar.gz
 fi
 
-cd jena-fuseki1-1.3.0
+cd jena-fuseki1-$FUSEKI_VERSION
 ./fuseki-server --config ../fuseki-assembler.ttl &
 until $(curl --output /dev/null --silent --head --fail http://localhost:3030); do
   printf '.'
