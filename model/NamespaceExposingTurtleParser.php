e01a7c8e233d9e5636ced94dee291096f66f52fe
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2017-03-21 15:22:59 +0200

    overwriting the turtle parser by rewriting the read, unread and peek operations, related to #573

--- model/NamespaceExposingTurtleParser.php
+++ model/NamespaceExposingTurtleParser.php
@@ -2,6 +2,9 @@
 
 class NamespaceExposingTurtleParser extends EasyRdf_Parser_Turtle
 {
+    private $bytePos = 0;
+    private $dataLength = null;
+
     /**
      * Returns the namespace prefixes as an array of prefix => URI
      * @return array $namespaces
@@ -9,5 +12,58 @@ class NamespaceExposingTurtleParser extends EasyRdf_Parser_Turtle
     public function getNamespaces()
     {
         return $this->namespaces;
+    }
+
+    /**
+     * A lot faster since we're now only reading the next 4 bytes 
+     * instead of the whole string.
+     * Gets the next character to be returned by read().
+     */
+    protected function peek()
+    {
+        if(!$this->dataLength) { $this->dataLength = strlen($this->data); }
+        if ($this->dataLength > $this->bytePos) {
+            $slice = substr($this->data, $this->bytePos, 4);
+            return mb_substr($slice, 0, 1, "UTF-8");
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Does not manipulate the data variable. Keeps track of the
+     * byte position instead.
+     * Read a single character from the input buffer.
+     * Returns -1 when the end of the file is reached.
+     */
+    protected function read()
+    {
+        $char = $this->peek();
+        if ($char == -1) {
+            return -1;
+        }
+        $this->bytePos += strlen($char);
+        // Keep tracks of which line we are on (0A = Line Feed)
+        if ($char == "\x0A") {
+            $this->line += 1;
+            $this->column = 1;
+        } else {
+            $this->column += 1;
+        }
+        return $char;
+    }
+
+    /**
+     * Steps back, restoring the previous character or statement read() to the input buffer
+     */
+    protected function unread($chars)
+    {
+        if ($this->column > 0) {
+            $this->column -= mb_strlen($chars, "UTF-8");
+            $this->bytePos -= strlen($chars);
+            if ($this->bytePos < 0) {
+                $this->bytePos = 0;
+            }
+        }
     }
 }
