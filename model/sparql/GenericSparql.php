7b084abb5c2490b84d0306685233c1fa3ff25aab
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2017-05-17 14:31:10 +0300

    showing matched altLabels without a language tag, fixes #601

--- model/sparql/GenericSparql.php
+++ model/sparql/GenericSparql.php
@@ -966,12 +966,11 @@ EOQ;
         $labelpriority = <<<EOQ
   FILTER(BOUND(?s))
   BIND(STR(SUBSTR(?hit,1,1)) AS ?pri)
-  BIND(STRLANG(STRAFTER(?hit, '@'), SUBSTR(STRBEFORE(?hit, '@'),2)) AS ?match)
+  BIND(IF((SUBSTR(STRBEFORE(?hit, '@'),1) != ?pri), STRLANG(STRAFTER(?hit, '@'), SUBSTR(STRBEFORE(?hit, '@'),2)), STRAFTER(?hit, '@')) AS ?match)
   BIND(IF((?pri = "1" || ?pri = "2") && ?match != ?label, ?match, ?unbound) as ?plabel)
   BIND(IF((?pri = "3" || ?pri = "4"), ?match, ?unbound) as ?alabel)
   BIND(IF((?pri = "5" || ?pri = "6"), ?match, ?unbound) as ?hlabel)
 EOQ;
-        
         $innerquery = $this->generateConceptSearchQueryInner($params->getSearchTerm(), $params->getLang(), $params->getSearchLang(), $props, $unique, $filterGraph);
         if ($params->getSearchTerm() === '*' || $params->getSearchTerm() === '') { 
           $labelpriority = ''; 
