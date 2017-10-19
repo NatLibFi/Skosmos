595415cf38842b0c8712450bbf69ff0b7530d1cc
Thomas Francart <thomas.francart@sparna.fr>
2017-10-19 09:23:25 +0200

    Cleaning, remove logs, rename variables and files in english

--- model/sparql/GenericSparql.php
+++ model/sparql/GenericSparql.php
@@ -595,13 +595,13 @@ EOQ;
     private function generateQueryConceptSchemesQuery($lang) {
         $fcl = $this->generateFromClause();
         $query = <<<EOQ
-SELECT ?cs ?label ?preflabel ?title ?domaine ?domaineLabel $fcl
+SELECT ?cs ?label ?preflabel ?title ?domain ?domainLabel $fcl
 WHERE {
  ?cs a skos:ConceptScheme .
  OPTIONAL{
-    ?cs dcterms:subject ?domaine.
-    ?domaine skos:prefLabel ?domaineLabel.
-    FILTER(langMatches(lang(?domaineLabel), '$lang'))
+    ?cs dcterms:subject ?domain.
+    ?domain skos:prefLabel ?domainLabel.
+    FILTER(langMatches(lang(?domainLabel), '$lang'))
 }
  OPTIONAL {
    ?cs rdfs:label ?label .
@@ -647,8 +647,8 @@ EOQ;
             }
             //ajout des dcterms:subject et leurs libellés dans le retour json
             if(isset($row->domaine)&&isset($row->domaineLabel)){
-                $conceptscheme['subject']['uri']=$row->domaine->getURI();
-                $conceptscheme['subject']['prefLabel']=$row->domaineLabel->getValue();
+                $conceptscheme['subject']['uri']=$row->domain->getURI();
+                $conceptscheme['subject']['prefLabel']=$row->domainLabel->getValue();
             }
 
             $ret[$row->cs->getURI()] = $conceptscheme;
