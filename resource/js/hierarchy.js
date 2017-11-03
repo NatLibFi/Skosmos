293a4333158c00032ffdfa88a251742f67f2f01a
Thomas Francart <thomas.francart@sparna.fr>
2017-11-03 10:50:23 +0100

    Beautifying hierarchy.js

--- resource/js/hierarchy.js
+++ resource/js/hierarchy.js
@@ -131,40 +131,43 @@ function attachTopConceptsToSchemes(schemes, currentNode, parentData) {
   var foundFirstLevel = false;
 
   for (var i = 0; i < schemes.length; i++) {
-      //search if top concept uri is equal to scheme uri on (first level)
+        //search if top concept uri is equal to scheme uri on (first level)
         if(schemes[i].uri===parentData[currentNode.uri].top){
           foundFirstLevel = true;
           if(Object.prototype.toString.call(schemes[i].children) !== '[object Array]' ) {
             schemes[i].children = [];
           }
+          // append to a first-level node that corresponds to a concept scheme
           schemes[i].children.push(currentNode);
-        // the hierarchy response contains the parent info before the topConcepts so it's a safe to open the first one without broaders 
-        if (!schemes[i].opened && !currentNode.broader) {
-          schemes[i].state = currentNode.state;
-          schemes.opened = true;
-        }
+          
+          // the hierarchy response contains the parent info before the topConcepts so it's a safe to open the first one without broaders 
+          if (!schemes[i].opened && !currentNode.broader) {
+            schemes[i].state = currentNode.state;
+            schemes.opened = true;
+          }
         
         } 
-      // search if top concept uri is equal to scheme children uri, if there are children(second Level)
+
+      // search if top concept uri is equal to scheme children uri, if there are children (second Level)
+      // this may be the case if schemes are organised by subjects : subjects will be first-level nodes, concepts schemes will be second level
       for (var h = 0; h <schemes[i].children.length; h++) {
-          console.log('scheme children: '+schemes[i].children[h].uri+' currentNodeUri: '+parentData[currentNode.uri].uri);
           if(schemes[i].children[h]){
-          if(schemes[i].children[h].uri===parentData[currentNode.uri].top){
+            if(schemes[i].children[h].uri===parentData[currentNode.uri].top) {
               if(Object.prototype.toString.call(schemes[i].children[h].children) !== '[object Array]' ) {
                 schemes[i].children[h].children = [];
               }
-          schemes[i].children[h].children.push(currentNode);
-        // the hierarchy response contains the parent info before the topConcepts so it's a safe to open the first one without broaders 
-        if (!schemes[i].children[h].opened && !currentNode.broader) {
-          schemes[i].state = currentNode.state;
-          schemes[i].children[h].state=schemes[i].state;
-          schemes[i].children[h].opened=true;
-          schemes[i].children[h].children.opened=true;
+              schemes[i].children[h].children.push(currentNode);
         
-        }
+              // the hierarchy response contains the parent info before the topConcepts so it's a safe to open the first one without broaders 
+              if (!schemes[i].children[h].opened && !currentNode.broader) {
+                // open the first-level node and second-level node
+            	schemes[i].state = currentNode.state;
+                schemes[i].children[h].state=schemes[i].state;
+                schemes[i].children[h].opened=true;
+                schemes[i].children[h].children.opened=true;
+              }
+            }
           }
-        }
-        
       }
        
   }
@@ -180,52 +183,54 @@ function attachTopConceptsToSchemes(schemes, currentNode, parentData) {
  * @param {Object} schemes 
  */
 function buildParentTree(uri, parentData, schemes) {
-  if (parentData === undefined || parentData === null) { return; }
+	console.log("buildParentTree");
+	if (parentData === undefined || parentData === null) { return; }
 
-  var loopIndex = 0, // for adding the last concept as a root if no better candidates have been found.
-    currentNode,
-    rootArray = (schemes.length > 1) ? schemes : [];
-    var domains=[];
+	var loopIndex = 0, // for adding the last concept as a root if no better candidates have been found.
+	currentNode,
+	rootArray = (schemes.length > 1) ? schemes : [];
 
-  for(var conceptUri in parentData) {
-    if (parentData.hasOwnProperty(conceptUri)) {
-      var branchHelper, 
-        exactMatchFound;
-      currentNode = createConceptObject(conceptUri, parentData[conceptUri]);
-      /* if a node has the property topConceptOf set it as the root node. 
-       * Or just setting the last node as a root if nothing else has been found 
-       */
-      if (parentData[conceptUri].top || ( loopIndex === Object.size(parentData)-1) && rootArray.length === 0 || !currentNode.parents && rootArray.length === 0) { 
-        if (rootArray.length === 0) {  
-          branchHelper = currentNode;
-        }
-        // if there are multiple concept schemes attach the topConcepts to the concept schemes
-        if (schemes.length > 1 && (parentData[conceptUri].top)) {
-          schemes = attachTopConceptsToSchemes(schemes, currentNode, parentData);
-          
-        }
-        else {
-          rootArray.push(currentNode);
-        }
-      }
-      if (exactMatchFound) { // combining branches if we have met a exact match during the previous iteration.
-        currentNode.children.push(branchHelper); 
-        branchHelper = undefined;
-        exactMatchFound = false;
-      }
-      // here we have iterated far enough to find the merging point of the trees.
-      if (branchHelper && parentData[branchHelper.uri].exact === currentNode.uri) {
-        exactMatchFound = true;
-      } 
-      setNode(currentNode);
-      loopIndex++;
-    }
-  }
+	for(var conceptUri in parentData) {
+		if (parentData.hasOwnProperty(conceptUri)) {
+			var branchHelper, 
+			exactMatchFound;
+			currentNode = createConceptObject(conceptUri, parentData[conceptUri]);
+			/* if a node has the property topConceptOf set it as the root node. 
+			 * Or just setting the last node as a root if nothing else has been found 
+			 */
+			if (parentData[conceptUri].top || ( loopIndex === Object.size(parentData)-1) && rootArray.length === 0 || !currentNode.parents && rootArray.length === 0) { 
+				if (rootArray.length === 0) {  
+					branchHelper = currentNode;
+				}
+				// if there are multiple concept schemes attach the topConcepts to the concept schemes
+				if (schemes.length > 1 && (parentData[conceptUri].top)) {
+					schemes = attachTopConceptsToSchemes(schemes, currentNode, parentData);
 
-  // Iterating over the nodes to make sure all concepts have their children set.
-  appendChildrenToParents();
-  // avoiding the issue with multiple inheritance by deep copying the whole tree object before giving it to jsTree
-  return JSON.parse(JSON.stringify(rootArray));
+				}
+				else {
+					rootArray.push(currentNode);
+				}
+			}
+			if (exactMatchFound) { // combining branches if we have met a exact match during the previous iteration.
+				currentNode.children.push(branchHelper); 
+				branchHelper = undefined;
+				exactMatchFound = false;
+			}
+			// here we have iterated far enough to find the merging point of the trees.
+			if (branchHelper && parentData[branchHelper.uri].exact === currentNode.uri) {
+				exactMatchFound = true;
+			} 
+			setNode(currentNode);
+			loopIndex++;
+		}
+	}
+
+	// Iterating over the nodes to make sure all concepts have their children set.
+	appendChildrenToParents();
+	// avoiding the issue with multiple inheritance by deep copying the whole tree object before giving it to jsTree
+	
+	// console.log(JSON.stringify(rootArray, null, 4));
+	return JSON.parse(JSON.stringify(rootArray));
 }
 
 function getConceptHref(conceptData) {
@@ -315,7 +320,7 @@ function getParams(node) {
   return $.param({'uri' : nodeId, 'lang' : clang});
 }
 
-function pickLabelFromScheme(scheme) {
+function pickLabel(scheme) {
   var label = '';
   if (scheme.prefLabel)
     label = scheme.prefLabel;
@@ -330,7 +335,7 @@ function schemeRoot(schemes) {
   var topArray = [];
   
   
-  // Step 1 : find domain list
+  // Step 1 : gather domain list
   var domains=[];
   for (var i = 0; i < schemes.length; i++) {
     if(schemes[i].subject != null) {
@@ -351,55 +356,71 @@ function schemeRoot(schemes) {
         }
     }
   }
-  
-  // console.log(domains);
 
   // Step 2 : create tree nodes for each domain
  
   for (var i = 0; i < domains.length; i++) {
     var theDomain = domains[i];
-    // Step 2.1 : create domain node without children
-    var domainObject = {
-      text: theDomain.prefLabel, 
-      a_attr : { "href" : vocab + '/' + lang + '/page/?uri=' + theDomain.uri, 'class': 'domain'},
-      uri: theDomain.uri,
-      children: [],
-      state: { opened: false } 
-    };
+    var theDomainLabel = pickLabel(theDomain);
+    
+    // avoid creating entries with empty labels
+    if(theDomainLabel != '') {        
+        // Step 2.1 : create domain node without children
+        var domainObject = {
+        		text: theDomainLabel, 
+        		a_attr : { "href" : vocab + '/' + lang + '/page/?uri=' + theDomain.uri, 'class': 'domain'},
+        		uri: theDomain.uri,
+        		children: [],
+        		state: { opened: false } 
+        };
 
-    // Step 2.2 : find the concept schemes in this domain and add them as children
-    for (var k = 0; k < schemes.length; k++) {
-        var theScheme = schemes[k];        
-        if((theScheme.subject) != null && (theScheme.subject.uri===theDomain.uri)) {
-            domainObject.children.push(
-                    {
-                      text:theScheme.prefLabel,
-                      a_attr:{ "href" : vocab + '/' + lang + '/page/?uri=' + theScheme.uri, 'class': 'scheme'},
-                      uri: theScheme.uri,
-                      children: true,
-                      state: { opened: false } 
-                    }
-            );
+        // Step 2.2 : find the concept schemes in this domain and add them as children
+        for (var k = 0; k < schemes.length; k++) {
+        	var theScheme = schemes[k];
+        	var theSchemeLabel = pickLabel(theScheme);
+        	
+        	// avoid creating entries with empty labels
+        	if(theSchemeLabel != '') { 
+	        	if((theScheme.subject) != null && (theScheme.subject.uri===theDomain.uri)) {
+	        		domainObject.children.push(
+	        				{
+	        					text: theSchemeLabel,
+	        					a_attr:{ "href" : vocab + '/' + lang + '/page/?uri=' + theScheme.uri, 'class': 'scheme'},
+	        					uri: theScheme.uri,
+	        					children: true,
+	        					state: { opened: false } 
+	        				}
+	        		);
+	        	}
+        	}
         }
+        topArray.push(domainObject);   	
     }
-    topArray.push(domainObject);
+
   }
 
-  // Step 3 : add the schemes without subjects
-   for (var k = 0; k < schemes.length; k++) {
-        var theScheme = schemes[k];        
-        if(theScheme.subject == null) {
-            topArray.push(
-                    {
-                      text:theScheme.prefLabel,
-                      a_attr:{ "href" : vocab + '/' + lang + '/page/?uri=' + theScheme.uri, 'class': 'scheme'},
-                      uri: theScheme.uri,
-                      children: true,
-                      state: { opened: false } 
-                    }
-            );
-        }
-    }
+  // Step 3 : add the schemes without any subjects after the subjects node
+  for (var k = 0; k < schemes.length; k++) {
+	  var theScheme = schemes[k]; 
+	  
+	  if(theScheme.subject == null) {		  
+		  // avoid creating entries with empty labels
+		  var theSchemeLabel = pickLabel(theScheme);
+		  if(theSchemeLabel != '') { 		  
+			  topArray.push(
+					  {
+						  text:theSchemeLabel,
+						  a_attr:{ "href" : vocab + '/' + lang + '/page/?uri=' + theScheme.uri, 'class': 'scheme'},
+						  uri: theScheme.uri,
+						  children: true,
+						  state: { opened: false } 
+					  }
+			  );
+		  }
+	  }
+  }
+  
+  // console.log(JSON.stringify(topArray, null, 4));
 
   return topArray;
 }
