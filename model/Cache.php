7607f8930fa8fc1e36b81c693501b2b6624c8a08
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-12 15:33:59 +0200

    Remove support for obsolete APC caching functions. Fixes #728

--- model/Cache.php
+++ model/Cache.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Wrapper class for key-value caching. Currently supports APC and APCu.
+ * Wrapper class for key-value caching. Currently supports only APCu.
  */
 class Cache
 {
@@ -10,9 +10,6 @@ class Cache
      * Wraps apc_fetch() & apcu_fetch()
      */
     public function fetch($key) {
-        if (function_exists('apc_fetch')) {
-            return apc_fetch($key);
-        }
         if (function_exists('apcu_fetch')) {
             return apcu_fetch($key);
         }
@@ -23,15 +20,12 @@ class Cache
      * Wraps apc_store() and apcu_store()
      */
     public function store($key, $value, $ttl=3600) {
-        if (function_exists('apc_store')) {
-            return apc_store($key, $value);
-        }
-        else if (function_exists('apcu_store')) {
+        if (function_exists('apcu_store')) {
             return apcu_store($key, $value, $ttl);
         }
     }
 
     public function isAvailable() {
-        return ((function_exists('apc_store') && function_exists('apc_fetch')) || (function_exists('apcu_store') && function_exists('apcu_fetch')));
+        return (function_exists('apcu_store') && function_exists('apcu_fetch'));
     }
 }
