6fbca9effcd6dda75f74a960fbdaa951fe3d3350
kouralex <1723419+kouralex@users.noreply.github.com>
2018-11-15 14:33:34 +0200

    #fixes #791; also update fuseki to 3.9.0

--- tests/init_fuseki.sh
+++ tests/init_fuseki.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 # Note: This script must be sourced from within bash, e.g. ". init_fuseki.sh"
 
-FUSEKI_VERSION=${FUSEKI_VERSION:-3.8.0}
+FUSEKI_VERSION=${FUSEKI_VERSION:-3.9.0}
 
 if [ "$FUSEKI_VERSION" = "SNAPSHOT" ]; then
-	# find out the latest snapshot version and its download URL by parsing Apache directory listings
-	snapshotdir="https://repository.apache.org/content/repositories/snapshots/org/apache/jena/jena-fuseki1/"
-	latestdir=$(wget -q -O- "$snapshotdir" | grep 'a href=' | cut -d '"' -f 2 | grep SNAPSHOT | tail -n 1)
-	FUSEKI_VERSION=$(basename "$latestdir")
-	fusekiurl=$(wget -q -O- "$latestdir" | grep 'a href=' | cut -d '"' -f 2 | grep 'distribution\.tar\.gz$' | tail -n 1)
+    # find out the latest snapshot version and its download URL by parsing Apache directory listings
+    snapshotdir="https://repository.apache.org/content/repositories/snapshots/org/apache/jena/apache-jena-fuseki/"
+    latestdir=$(wget -q -O- "$snapshotdir" | grep 'a href=' | cut -d '"' -f 2 | grep SNAPSHOT | tail -n 1)
+    FUSEKI_VERSION=$(basename "$latestdir")
+    fusekiurl=$(wget -q -O- "$latestdir" | grep 'a href=' | cut -d '"' -f 2 | grep '\.tar\.gz$' | tail -n 1)
 else
-	fusekiurl="https://repository.apache.org/content/repositories/releases/org/apache/jena/jena-fuseki1/$FUSEKI_VERSION/jena-fuseki1-$FUSEKI_VERSION-distribution.tar.gz"
+    fusekiurl="https://repository.apache.org/content/repositories/releases/org/apache/jena/apache-jena-fuseki/$FUSEKI_VERSION/apache-jena-fuseki-$FUSEKI_VERSION.tar.gz"
 fi
 
-if [ ! -f "jena-fuseki1-$FUSEKI_VERSION/fuseki-server" ]; then
-  echo "fuseki server file not found - downloading it"
-  wget --output-document=fuseki-dist.tar.gz "$fusekiurl"
-  tar -zxvf fuseki-dist.tar.gz
+if [ ! -f "apache-jena-fuseki-$FUSEKI_VERSION/fuseki-server" ]; then
+    echo "fuseki server file not found - downloading it"
+    wget --output-document=fuseki-dist.tar.gz "$fusekiurl"
+    tar -zxvf fuseki-dist.tar.gz
 fi
 
-cd "jena-fuseki1-$FUSEKI_VERSION"
+cd "apache-jena-fuseki-$FUSEKI_VERSION"
 ./fuseki-server --port=13030 --config ../fuseki-assembler.ttl &
 until curl --output /dev/null --silent --head --fail http://localhost:13030; do
-  printf '.'
-  sleep 2
+    printf '.'
+    sleep 2
 done
 
 for fn in ../test-vocab-data/*.ttl; do
-  name=$(basename "${fn}" .ttl)
-  $(./s-put http://localhost:13030/ds/data "http://www.skosmos.skos/$name/" "$fn")
+    name=$(basename "${fn}" .ttl)
+    $(./bin/s-put http://localhost:13030/ds/data "http://www.skosmos.skos/$name/" "$fn")
 done
 
 cd ..
+
