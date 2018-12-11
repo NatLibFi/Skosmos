7b0cbc5143596d440b55975f38f0efb555682afa
Osma Suominen <osma.suominen@helsinki.fi>
2018-12-11 10:20:21 +0200

    Add failing unit test for #805

--- tests/VocabularyTest.php
+++ tests/VocabularyTest.php
@@ -194,6 +194,15 @@ class VocabularyTest extends \PHPUnit\Framework\TestCase
   }
 
   /**
+   * @covers Vocabulary::getAlphabet
+   */
+  public function testGetAlphabetIssue805() {
+    $vocab = $this->model->getVocabulary('collation');
+    $alpha = $vocab->getAlphabet('fi');
+    $this->assertEquals(array("A", "T", "Ä"), $alpha);
+  }
+
+  /**
    * @covers Vocabulary::getInfo
    * @covers Vocabulary::parseVersionInfo
    */
