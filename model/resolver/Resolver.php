4455a1563df9c9e850b70af99ae5b52879312069
Osma Suominen <osma.suominen@helsinki.fi>
2020-02-05 17:24:30 +0200

    Initial implementation of LDF lookups for Wikidata. Fixes #912

--- model/resolver/Resolver.php
+++ model/resolver/Resolver.php
@@ -3,6 +3,10 @@
 class Resolver
 {
 
+    private function startsWith(string $prefix, string $target) : bool {
+        return strpos($target, $prefix) === 0;
+    }
+
     /**
      * Resolve the URI using the most appropriate resolver and return the
      * result as an ExternalResource.
@@ -10,7 +14,11 @@ class Resolver
      * @return EasyRdf\Resource
      */
     public function resolve(string $uri, int $timeout): ?EasyRdf\Resource {
-        $res = new LinkedDataResource($uri);
+        if ($this->startsWith('http://www.wikidata.org/entity/', $uri)) {
+            $res = new LDFResource($uri, 'https://query.wikidata.org/bigdata/ldf');
+        } else {
+            $res = new LinkedDataResource($uri);
+        }
         return $res->resolve($timeout);
     }
 }
