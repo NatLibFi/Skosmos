5202c74f6cd98f2acf514cece8270fa38d134299
Osma Suominen <osma.suominen@helsinki.fi>
2020-02-07 11:31:15 +0200

    implement logging for resolvers (and turn RemoteResource into an abstract class)

--- model/resolver/Resolver.php
+++ model/resolver/Resolver.php
@@ -2,6 +2,15 @@
 
 class Resolver
 {
+    private $model;
+
+    /**
+     * Initializes the Resolver object
+     */
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
 
     private function startsWith(string $prefix, string $target) : bool {
         return strpos($target, $prefix) === 0;
@@ -15,11 +24,11 @@ class Resolver
      */
     public function resolve(string $uri, int $timeout): ?EasyRdf\Resource {
         if (preg_match('|http://id.loc.gov/[^/]+/[^/]+/.+|', $uri)) {
-            $res = new LOCResource($uri);
+            $res = new LOCResource($this->model, $uri);
         } elseif ($this->startsWith('http://www.wikidata.org/entity/', $uri)) {
-            $res = new WDQSResource($uri);
+            $res = new WDQSResource($this->model, $uri);
         } else {
-            $res = new LinkedDataResource($uri);
+            $res = new LinkedDataResource($this->model, $uri);
         }
         return $res->resolve($timeout);
     }
