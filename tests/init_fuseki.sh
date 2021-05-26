213575cb3fde82e7d5d0edbbca90d58bb1ad1d5f
Osma Suominen <osma.suominen@helsinki.fi>
2021-05-26 15:05:23 +0300

    Upgrade to Fuseki 4.0.0 for running the unit tests

--- tests/init_fuseki.sh
+++ tests/init_fuseki.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Note: This script must be sourced from within bash, e.g. ". init_fuseki.sh"
 
-FUSEKI_VERSION=${FUSEKI_VERSION:-3.14.0}
+FUSEKI_VERSION=${FUSEKI_VERSION:-4.0.0}
 
 if [ "$FUSEKI_VERSION" = "SNAPSHOT" ]; then
     # find out the latest snapshot version and its download URL by parsing Apache directory listings
