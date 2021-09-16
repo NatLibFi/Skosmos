bf934254a4dbac8022ad2b96009516eeeb84e3bb
Vainonen <okko.vainonen@helsinki.fi>
2021-09-16 16:27:46 +0300

    fixed getPluginArray function and added test

--- tests/VocabularyConfigTest.php
+++ tests/VocabularyConfigTest.php
@@ -516,6 +516,16 @@ class VocabularyConfigTest extends PHPUnit\Framework\TestCase
   }
 
   /**
+   * @covers VocabularyConfig::getPluginArray
+   */
+  public function testGetUnorderedVocabularyPlugins() {
+    $vocab = $this->model->getVocabulary('paramPluginOrderTest');
+    $plugins = $vocab->getConfig()->getPluginArray();
+    $arrayElements = ["plugin2", "Bravo", "imaginaryPlugin", "plugin1", "alpha", "charlie", "plugin3"];
+    $this->assertEquals(sort($arrayElements), sort($plugins));
+  }
+
+  /**
    * @covers VocabularyConfig::getPropertyOrder
    */
   public function testGetPropertyOrderNotSet() {
