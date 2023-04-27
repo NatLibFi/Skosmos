b99858e67a59631b2a53fdd69908efaff098af91
Unni Kohonen <unni.kohonen@aalto.fi>
2023-04-27 11:43:47 +0300

    Add vue components for vocab statistics

---
+++ resource/js/vocab-counts.js
@@ -0,0 +1,54 @@
+const resourceCountsApp = Vue.createApp({
+  data () {
+    return {
+      concepts: {},
+      subTypes: {},
+      conceptGroups: {}
+    }
+  },
+  mounted () {
+    fetch('https://api.finto.fi/rest/v1/yso/vocabularyStatistics?lang=fi') /* What should this be? */
+      .then(data => {
+        return data.json()
+      })
+      .then(data => {
+        console.log('resource count', data)
+        this.concepts = data.concepts
+        this.subTypes = data.subTypes
+        this.conceptGroups = data.conceptGroups
+      })
+  },
+  template: `
+    <h3 class="fw-bold py-3">Resource counts by type</h3>
+    <table class="table" id="counts">
+      <tbody>
+        <tr><th class="versal">Type</th><th class="versal">Count</th></tr>
+        <resource-counts :concepts="concepts" :subTypes="subTypes" :conceptGroups="conceptGroups"></resource-counts>
+      </tbody>
+    </table>
+  `
+})
+
+resourceCountsApp.component('resource-counts', {
+  props: ['concepts', 'subTypes', 'conceptGroups'],
+  template: `
+    <tr>
+      <td>{{ concepts.label }}</td>
+      <td>{{ concepts.count }}</td>
+    </tr>
+    <tr v-for="st in subTypes">
+      <td>* {{ st.label }}</td>
+      <td>{{ st.count }}</td>
+    </tr>
+    <tr>
+      <td>* Käytöstä poistettu käsite</td>
+      <td>{{ concepts.deprecatedCount }}</td>
+    </tr>
+    <tr>
+      <td>{{ conceptGroups.label }}</td>
+      <td>{{ conceptGroups.count }}</td>
+    </tr>
+  `
+})
+
+resourceCountsApp.mount('#resource-counts')
