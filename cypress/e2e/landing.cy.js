044e593d45c98da4ec7b5243faa9c3133b294965
Osma Suominen <osma.suominen@helsinki.fi>
2023-05-24 11:11:05 +0300

    expand Cypress tests to cover all current page types + set baseUrl

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -1,11 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Landing page', () => {
+  it('links to vocabulary home', () => {
     // go to the Skosmos front page
-    cy.visit('http://localhost/Skosmos')
+    cy.visit('/')
     // click on the first vocabulary in the list
     cy.get('#vocabulary-list').find('a').first().click()
     // check that we are still within the Skosmos URL space
-    cy.url().should('include', 'http://localhost/Skosmos/')
+    cy.url().should('include', Cypress.config().baseUrl)
     // check that we are on the vocab home page
     cy.get('#vocab-info')
   })
---
+++ cypress/e2e/vocab-search.cy.js
@@ -0,0 +1,3 @@
+describe('Vocabulary search page', () => {
+  it('no-op test', () => {})
+})
