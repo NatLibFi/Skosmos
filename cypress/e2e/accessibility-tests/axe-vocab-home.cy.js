823ffdedbe3c5513b7d2b3865b257a95ba2749db
Mika Vaara <mika.vaara@helsinki.fi>
2023-06-01 15:20:51 +0300

    Added skip option to Axe tests

--- cypress/e2e/accessibility-tests/axe-vocab-home.cy.js
+++ cypress/e2e/accessibility-tests/axe-vocab-home.cy.js
@@ -1,6 +1,9 @@
 import { accessibilityTestRunner } from '../../support/accessibility.js'
 
-describe('Check accessibility of the vocab-home page', () => {
+/* If you want the test to be skipped, add a skip command after the describe part:
+    - test enabled: describe('Check accessibility of ...
+    - test to be skipped: describe.skip('Check accessibility of ... */
+describe.skip('Check accessibility of the vocab-home page', () => {
   before(() => {
     cy.visit('/agrovoc/fi/')
     cy.injectAxe()
--- cypress/e2e/accessibility-tests/axe-vocab-search.cy.js
+++ cypress/e2e/accessibility-tests/axe-vocab-search.cy.js
@@ -1,3 +1,6 @@
-describe('Check accessibility of the vocabulary search page', () => {
+/* If you want the test to be skipped, add a skip command after the describe part:
+    - test enabled: describe('Check accessibility of ...
+    - test to be skipped: describe.skip('Check accessibility of ... */
+describe.skip('Check accessibility of the vocabulary search page', () => {
   it('no-op test', () => {})
 })
