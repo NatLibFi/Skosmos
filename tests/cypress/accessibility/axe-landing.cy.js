8e27e7653c0da27a7ac946fd3926407c078368be
Mika Vaara <mika.vaara@helsinki.fi>
2023-08-30 18:20:49 +0300

    Unnecessary extra line removed

--- tests/cypress/accessibility/axe-landing.cy.js
+++ tests/cypress/accessibility/axe-landing.cy.js
@@ -1,5 +1,6 @@
 import { accessibilityTestRunner } from '../support/accessibility.js'
 import 'cypress-axe';
+
 /* If you want the test to be skipped, add a skip command after the describe part:
     - test enabled: describe('Check accessibility of ...
     - test to be skipped: describe.skip('Check accessibility of ... */
