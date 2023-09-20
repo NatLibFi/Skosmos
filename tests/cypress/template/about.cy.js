02cae2c3e78025edce0a44ad99e47e93d1d83770
Mika Vaara <mika.vaara@helsinki.fi>
2023-09-20 12:04:27 +0300

    For testing purposes added Mika's test content and test itself

--- tests/cypress/template/about.cy.js
+++ tests/cypress/template/about.cy.js
@@ -2,6 +2,8 @@ describe('About page', () => {
   it('Contains version number information', () => {
     // go to the Skosmos about page
     cy.visit('/en/about')
+    // check if the page contains "Mikan testiteksti"
+    cy.contains('Mikan testiteksti')
     // check that the version information should mention it's Skosmos and something that looks like version number
     cy.get('#version > p').invoke('text').should('match', /.*Skosmos.*[0-9]/)
   })
