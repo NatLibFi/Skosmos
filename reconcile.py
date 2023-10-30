8bb3ce6fb501e7628150497b41e8178e1acc3838
Unni Kohonen <unni.kohonen@aalto.fi>
2023-10-30 18:27:13 +0200

    Add language to routes

--- reconcile.py
+++ reconcile.py
@@ -13,7 +13,7 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-api_base_url = 'http://api.dev.finto.fi/rest/v1/'
+api_base_url = 'https://api.dev.finto.fi/rest/v1/'
 
 def jsonpify(obj):
     """
@@ -28,11 +28,11 @@ def jsonpify(obj):
         return jsonify(obj)
 
 
-def search(raw_query, vocid, limit, query_type=""):
+def search(raw_query, vocid, limit, lang, query_type=""):
     print("search", raw_query, query_type)
 
-    params = {"query": raw_query + "*", "maxhits": limit, "type": query_type, "unique": "true"}
-    search_results = requests.get(api_base_url + vocid + '/search', params=params).json()['results']
+    params = {"query": raw_query + "*", "maxhits": limit, "type": query_type, "unique": "true", "lang": lang}
+    search_results = requests.get(api_base_url + vocid + "/search/", params=params).json()['results']
 
     results = [{'id': res['uri'],
                 'name': res['prefLabel'],
@@ -46,18 +46,18 @@ def search(raw_query, vocid, limit, query_type=""):
     return results
 
 
-def metadata(vocid):
-    vocab = requests.get(api_base_url + vocid)
+def metadata(vocid, lang):
+    vocab = requests.get(api_base_url + vocid + "/", params={"lang": lang})
     title = vocab.json()['title']
     concept_schemes = vocab.json()['conceptschemes']
 
-    types = requests.get(api_base_url + vocid + '/types').json()['types']
+    types = requests.get(api_base_url + vocid + "/types/", params={"lang": lang}).json()['types']
     query_types = [{'id': type['uri'],
                     'name': type['label']}
                     for type in types]
 
     service_metadata = {
-        "name": "Skosmos reconciliation service for " + title,
+        "name": "Reconciliation service for " + title,
         "identifierSpace": concept_schemes[0]["uri"],
         "schemaSpace": "",
         "defaultTypes": query_types,
@@ -67,11 +67,11 @@ def metadata(vocid):
         "suggest": {
             "entity": {
                 "service_path": "/suggest/entity",
-                "service_url": request.url_root + vocid + "/reconcile"
+                "service_url": request.url_root + vocid + "/" + lang + "/reconcile"
             }
         },
         "preview": {
-            "url": request.url_root + vocid + "/reconcile/preview?id={{id}}",
+            "url": request.url_root + vocid + "/" + lang + "/reconcile/preview?id={{id}}",
             "width": 300,
             "height": 300
         }
@@ -79,8 +79,8 @@ def metadata(vocid):
 
     return service_metadata
 
-@app.route("/<vocid>/reconcile", methods=['POST', 'GET'])
-def reconcile(vocid):
+@app.route("/<vocid>/<lang>/reconcile", methods=['POST', 'GET'])
+def reconcile(lang, vocid):
     # If a 'queries' parameter is supplied then it is a dictionary
     # of (key, query) pairs representing a batch of queries. We
     # should return a dictionary of (key, results) pairs.
@@ -92,32 +92,32 @@ def reconcile(vocid):
         for (key, query) in queries.items():
             qtype = query.get('type')
             limit = query.get('limit')
-            data = search(query['query'], vocid=vocid, limit=limit, query_type=qtype)
+            data = search(query['query'], vocid=vocid, limit=limit, lang=lang, query_type=qtype)
             results[key] = {"result": data}
         return jsonpify(results)
     # If no 'queries' parameter is supplied then
     # we should return the service metadata.
-    return jsonpify(metadata(vocid))
+    return jsonpify(metadata(vocid, lang))
 
-@app.route("/<vocid>/reconcile/suggest/entity", methods=['GET'])
-def suggest(vocid):
+@app.route("/<vocid>/<lang>/reconcile/suggest/entity", methods=['GET'])
+def suggest(vocid, lang):
     prefix = request.args.get("prefix")
     cursor = int(request.args.get("cursor")) if request.args.get("cursor") else 0
     limit = cursor + 20
 
-    result = search(prefix, vocid=vocid, limit=limit)
-    print(result)
+    result = search(prefix, vocid=vocid, limit=limit, lang=lang)
 
     results = [{"id": res["id"], "name": res["name"], "notable": res["type"]} for res in result]
     return {"result": results[cursor:]}
 
-@app.route("/<vocid>/reconcile/preview", methods=['GET'])
-def preview(vocid):
+@app.route("/<vocid>/<lang>/reconcile/preview", methods=['GET'])
+def preview(vocid, lang):
     uri = request.args.get("id")
 
-    prefLabel = requests.get(api_base_url + vocid + "/label", params={"uri": uri}).json()["prefLabel"]
-    broader = requests.get(api_base_url + vocid + "/broader", params={"uri": uri}).json()["broader"]
-    narrower = requests.get(api_base_url + vocid + "/narrower", params={"uri": uri}).json()["narrower"]
+    params = {"uri": uri, "lang": lang}
+    prefLabel = requests.get(api_base_url + vocid + "/label/", params=params).json()["prefLabel"]
+    broader = requests.get(api_base_url + vocid + "/broader/", params=params).json()["broader"]
+    narrower = requests.get(api_base_url + vocid + "/narrower/", params=params).json()["narrower"]
     
     return render_template("preview.html", uri=uri, prefLabel=prefLabel, broader=broader, narrower=narrower)
 
