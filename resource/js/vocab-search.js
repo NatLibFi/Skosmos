6778dde18cf3cecc79947db111cbe5a09fca45a5
Joeli Takala <joeli.takala@helsinki.fi>
2023-11-16 12:59:56 +0200

    Basic search functionality

--- resource/js/vocab-search.js
+++ resource/js/vocab-search.js
@@ -5,20 +5,45 @@ const vocabSearch = Vue.createApp({
   data () {
     return {
       languages: [],
-      selectedLanguage: {}
+      selectedLanguage: null,
+      searchTerm: null,
+      autoCompeteResults: []
     }
   },
   mounted () {
       this.languages = SKOSMOS.languageOrder
       this.selectedLanguage = SKOSMOS.content_lang
   },
+  methods: {
+    autoComplete() {
+      if (!this.searchTerm) return;
+      console.log("Auto complete: " + this.searchTerm)
+    },
+    gotoSearchPage() {
+        if (!this.searchTerm) return;
+
+        var currentVocab = SKOSMOS.vocab + "/" + SKOSMOS.lang + "/"
+        var vocabHref = window.location.href.substring(0, window.location.href.lastIndexOf(SKOSMOS.vocab)) + currentVocab
+        var langParam = "&clang=" + SKOSMOS.content_lang
+        if (this.selectedLanguage == 'all') langParam += "&anylang=on"
+        var searchUrl = vocabHref + "search?q=" + this.searchTerm + langParam
+
+        location.href = searchUrl;
+    },
+    changeLang() {
+        console.log(this.selectedLanguage);
+        // Partial page load to change content language
+        return;
+    }
+  },
   template: `
     <div>
-      <input v-model="searchTerm" @input="performSearch" placeholder="Search...">
-      <select v-model="selectedLanguage" @change="performSearch">
+      <select v-model="selectedLanguage" @change="changeLang">
         <option v-for="lang in languages" v-bind:value="lang" >{{ lang }}</option>
         <option value="all">All</option>
       </select>
+      <input v-model="searchTerm" @input="autoComplete" @keyup.enter="gotoSearchPage" placeholder="Search...">
+      <button @click="gotoSearchPage">Suurennuslasi</button>
     </div>
   `
 })
