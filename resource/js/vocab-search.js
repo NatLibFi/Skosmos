d5996d0382f922d8ad007fcdc51e295046ccfdce
Joeli Takala <joeli.takala@helsinki.fi>
2023-11-22 12:52:45 +0200

    Changed search form to div element to avoid the default form submit behavior

--- resource/js/vocab-search.js
+++ resource/js/vocab-search.js
@@ -5,7 +5,7 @@ const vocabSearch = Vue.createApp({
   data () {
     return {
       languages: [],
-      selectedLanguage: "",
+      selectedLanguage: null,
       searchTerm: null,
       autoCompeteResults: []
     }
@@ -36,14 +36,14 @@ const vocabSearch = Vue.createApp({
   },
   template: `
     <div class="d-flex mb-2 my-auto ms-auto">
-    <form class="input-group" id="search-wrapper">
+    <div class="input-group" id="search-wrapper">
       <select v-model="selectedLanguage" @change="changeLang" class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown-item" aria-expanded="false">
         <option class="dropdown-item" v-for="lang in languages" v-bind:value="lang" >{{ lang }}</option>
         <option class="dropdown-item" value="all">All</option>
       </select>
       <input v-model="searchTerm" @input="autoComplete" @keyup.enter="gotoSearchPage" type="search" class="form-control" aria-label="Text input with dropdown button" placeholder="Search...">
       <button @click="gotoSearchPage" id="search-button" class="btn btn-outline-secondary"><i class="fa-solid fa-magnifying-glass"/></button>
-    </form>
+    </div>
     </div>
   `
 })
