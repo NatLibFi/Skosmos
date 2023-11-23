1506813825eba53f2e6abe23f16f58a480a88859
Mika Vaara <mika.vaara@helsinki.fi>
2023-11-23 14:20:50 +0200

    Ready e2e test according to the definitions in the issue #1548

--- tests/cypress/e2e/fp-lang-yso-clang-result-vocab.cy.js
+++ tests/cypress/e2e/fp-lang-yso-clang-result-vocab.cy.js
@@ -1,38 +1,25 @@
 describe('Front page -> lang -> vocab -> cLang -> search -> concept page', () => {
   before(() => {
     cy.visit('/fi');
-    cy.wait(2000);
   });
 
   it('should change UI language to English, select YSO from the list and then navigate to YSO concept page', () => {
     // Click on the language "button" to switch UI lang to English
     cy.get('#language-en').click();
-    cy.wait(1000);
 
     // Select "YSO - Yleinen suomalainen ontologia (arkeologia)" from the vocabulary list and go to the vocab page
     cy.contains('.list-group-item a', 'YSO - Yleinen suomalainen ontologia (arkeologia)').click();
-    cy.wait(1000);
 
     // Confirm that we are on the correct page (yso)
     cy.url().should('include', '/yso/en/');
-    cy.wait(1000);
-  });
 
-  it('should select "fi" language (only for the search) and perform a search', () => {
-    // Land on the page /yso/en/
-    cy.visit('/yso/en/');
-    cy.wait(1000);
-
-    // Open the language drop-down and select "fi"
+    // Open the language drop-down and select "fi" for the search language
     cy.get('.btn.btn-outline-secondary.dropdown-toggle').select('fi');
-    cy.wait(1000);
 
     // Write a search term "arkeologia" and press enter
     cy.get('.form-control').type('arkeologia').type('{enter}');
-    cy.wait(3000);
 
     // Check that the search result page contains "arkeologia"
     cy.get('.search-result-term a').should('include.text', 'arkeologia');
   });
-  
 });
