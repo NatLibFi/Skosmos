5e7e2f12ca14537a8f80ecfafab9e4bf7ec91aa0
Mika Vaara <mika.vaara@helsinki.fi>
2024-01-30 13:15:39 +0200

    The contrast issue on the About page has been fixed

--- tests/cypress/accessibility/axe-feedback.cy.js
+++ tests/cypress/accessibility/axe-feedback.cy.js
@@ -4,7 +4,7 @@ import 'cypress-axe';
 /* If you want the test to be skipped, add a skip command after the describe part:
     - test enabled: describe('Check accessibility of ...
     - test to be skipped: describe.skip('Check accessibility of ... */
-describe.skip('Check accessibility of the feedback page', () => {
+describe('Check accessibility of the feedback page', () => {
   before(() => {
     cy.visit('/fi/feedback')
     cy.injectAxe()
