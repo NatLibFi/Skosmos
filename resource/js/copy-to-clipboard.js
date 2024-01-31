30f5175d4a282bc78047b8f48c9b6b9ecad40e20
Osma Suominen <osma.suominen@helsinki.fi>
2024-01-31 13:02:01 +0200

    initial copy to clipboard functionality for the concept page

---
+++ resource/js/copy-to-clipboard.js
@@ -0,0 +1,26 @@
+// function for copying the content from a specific element (by id) to the clipboard
+async function copyToClipboard (id) {
+  const copyElem = document.getElementById(id)
+  const sel = window.getSelection()
+  const range = document.createRange()
+  range.selectNodeContents(copyElem)
+  sel.removeAllRanges()
+  sel.addRange(range)
+
+  try {
+    await navigator.clipboard.writeText(copyElem.innerText)
+  } catch (err) {
+    console.log('Failed to copy text to clipboard: ', err)
+  }
+}
+
+// register the copyToClipboard function as event an handler for the copy buttons
+const copyPrefElem = document.getElementById('copy-preflabel')
+if (copyPrefElem) {
+  copyPrefElem.addEventListener('click', () => copyToClipboard('concept-preflabel'))
+}
+
+const copyUriElem = document.getElementById('copy-uri')
+if (copyUriElem) {
+  copyUriElem.addEventListener('click', () => copyToClipboard('concept-uri'))
+}
