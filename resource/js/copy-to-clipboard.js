396932feb918c909f003cd51d3da73d5422d26d4
Osma Suominen <osma.suominen@helsinki.fi>
2024-01-31 17:12:30 +0200

    simplify JS code, avoiding async/await proliferation

--- resource/js/copy-to-clipboard.js
+++ resource/js/copy-to-clipboard.js
@@ -1,5 +1,5 @@
 // function for copying the content from a specific element (by id) to the clipboard
-async function copyToClipboard (id) {
+function copyToClipboard (id) {
   const copyElem = document.getElementById(id)
   const sel = window.getSelection()
   const range = document.createRange()
@@ -7,28 +7,17 @@ async function copyToClipboard (id) {
   sel.removeAllRanges()
   sel.addRange(range)
 
-  try {
-    await navigator.clipboard.writeText(copyElem.innerText)
-  } catch (err) {
-    console.log('Failed to copy text to clipboard: ', err)
-  }
+  navigator.clipboard.writeText(copyElem.innerText).catch((err) =>
+    console.error('Failed to copy text to clipboard: ', err))
 }
 
 // register the copyToClipboard function as event an handler for the copy buttons
 const copyPrefElem = document.getElementById('copy-preflabel')
 if (copyPrefElem) {
-  copyPrefElem.addEventListener('click', () => {
-    (async () => {
-      await copyToClipboard('concept-preflabel')
-    })()
-  })
+  copyPrefElem.addEventListener('click', () => copyToClipboard('concept-preflabel'))
 }
 
 const copyUriElem = document.getElementById('copy-uri')
 if (copyUriElem) {
-  copyUriElem.addEventListener('click', () => {
-    (async () => {
-      await copyToClipboard('concept-preflabel')
-    })()
-  })
+  copyUriElem.addEventListener('click', () => copyToClipboard('concept-uri'))
 }
