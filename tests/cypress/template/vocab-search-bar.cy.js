e8963da2ea7f7cdd16e2ce80a942d67fd387dab6
Joeli Takala <joeli.takala@helsinki.fi>
2024-02-01 17:23:31 +0200

    Added cypress tests

--- tests/cypress/template/vocab-search-bar.cy.js
+++ tests/cypress/template/vocab-search-bar.cy.js
@@ -34,4 +34,28 @@ describe('Vocab search bar', () => {
     cy.url().should('include', 'search?q=Katt');
     cy.url().should('include', 'anylang=on');
   })
+
+  it('Writing in the text field triggers the autocomplete results list', () => {
+    // go to YSO vocab front page
+    cy.visit('/yso/fi/')
+
+    cy.get('#search-field').type('kas'); // perform autocomplete search
+    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible').children().should('have.length.greaterThan', 4);
+     cy.get('#search-autocomplete-results').within(() => {
+        cy.get('li').eq(0).click(); // results should be links that take to a Skosmos page
+        cy.url().should('include', '/page/')
+      })
+  })
+
+  it('No results message is displayed if no results are found', () => {
+    // go to YSO vocab front page
+    cy.visit('/yso/en/')
+
+    cy.get('#search-field').type('kissa'); // even if the search yields no results, there shoulde a single line in the result list
+    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible').children().should('have.length.greaterThan', 0);
+    cy.get('#search-autocomplete-results').within(() => {
+        cy.get('li').eq(0).invoke('text').should('contain', 'No results') // the single result should display a no results message
+      })
+  })
+
 })
