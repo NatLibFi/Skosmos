8ce89a3d719c4a74f3924e605f57f3285340120a
Mika Vaara <mika.vaara@helsinki.fi>
2024-03-07 16:55:49 +0200

    Accessibility tests (WCAG 2.1 level AA) for the feedback page. Dropdown menu test is missing because Cypress cannot open native select-boxes.

--- tests/cypress/accessibility/axe-feedback.cy.js
+++ tests/cypress/accessibility/axe-feedback.cy.js
@@ -1,94 +1,47 @@
 import { accessibilityTestRunner } from '../support/accessibility.js'
-import 'cypress-axe';
+import 'cypress-axe'
 
 /* If you want the test to be skipped, add a skip command after the describe part:
     - test enabled: describe('Check accessibility of ...
     - test to be skipped: describe.skip('Check accessibility of ... */
 
-function fillInFormFields() {
-  const name = 'John Smith';
-  cy.get('#name-input').type(name);
-  cy.get('#name-input').invoke('val').then((nameFieldValue) => {
-    cy.log('The name is:', nameFieldValue);
-  });
+// To avoid redundancy, the function offering input values for fields that are needed more than once
+function fillInFormFields () {
+  // Input for the name field
+  const name = 'John Smith'
+  cy.get('#name-input').type(name)
 
-  const eMail = 'john.smith@testing.dom';
-  cy.get('#email-input').type(eMail);
-  cy.get('#email-input').invoke('val').then((eMailFieldValue) => {
-    cy.log('The e-mail is:', eMailFieldValue);
-  });
+  // Input for the email field
+  const eMail = 'john.smith@testing.dom'
+  cy.get('#email-input').type(eMail)
 
-  const subject = 'Test Subject';
-  cy.get('#subject-input').type(subject);
-  cy.get('#subject-input').invoke('val').then((subjectValue) => {
-    cy.log('The subject is:', subjectValue);
-  });
+  // Input for the subject field
+  const subject = 'Test Subject'
+  cy.get('#subject-input').type(subject)
 
-  const message = 'Test Subject';
-  cy.get('#message-input').type(message);
-  cy.get('#message-input').invoke('val').then((messageValue) => {
-    cy.log('The subject is:', messageValue);
-  });
+  // Input for the message field
+  const message = 'Test message'
+  cy.get('#message-input').type(message)
 }
-describe('Accessibility tests for dropdown menu', () => {
+
+describe('Testing the accessibility of the form', () => {
   before(() => {
     cy.visit('/fi/feedback')
-  });
 
-  it('should have accessible dropdown menu', () => {
-
-    // Listataan lapsielementit
-    cy.get('#vocab-select').children().each(($el) => {
-      cy.log($el[0].tagName);
-      cy.log($el.text());
-    });
-    
-    // Haetaan testin vuoksi "oletus" ilman aiempia kutsuja
-    cy.get('#vocab-select').invoke('val').then((value) => {
-      cy.get('#vocab-select').find(`option[value="${value}"]`).invoke('text').then((text) => {
-        cy.log('Oletus', text);
-      });
-    });
-
-    cy.get('#vocab-select').invoke('val').then((value) => {
-      cy.log('Selected:', value);
-    });
-    
-    // Jos tätä ei kutsuta, tulee alempana olevaan (X) "Ei tiettyyn sanastoon" eli ensimmäinen option, muuten tulee tässä selectoidun tiedot
-    // Mielenkiintoisen implisiittistä toimintaa. Parempi olisi jos tekisi vain niin kuin pyydetään.
-    // Lopulta kuitenkaan nämä eivät vaikuttane siihen, miksi drop-down ei aukea, mutta auttamat varmistamaan, että
-    // elementtiin päästään kiinni
-    // cy.get('#vocab-select').select('yso');
-    cy.get('#vocab-select').select('test');
-    
-    // X Oletustietoja lisää, riippuen yllä tehdyistä toimista
-    cy.get('#vocab-select').invoke('val').then((value) => {
-      cy.get('#vocab-select').find(`option[value="${value}"]`).invoke('text').then((text) => {
-        cy.log('Oletustietoja implisiittisin argumetein:', text);
-      });
-    });
-
-    cy.screenshot('dropdown-opened');
-
-    cy.injectAxe();
-    accessibilityTestRunner();
-  });
-});
-
-
-describe('Testataan itse formia ennen submittausta', () => {
-  before(() => {
-    cy.visit('/fi/feedback')
+    // Call the function that fills in the form fields
     fillInFormFields()
     cy.injectAxe()
   })
   accessibilityTestRunner()
 })
 
-describe('Testataan sivua, joka tulee formin syötön jälkeen', () => {
+describe('Testing the accessibility of the thank you page after submitting the filled form', () => {
   before(() => {
     cy.visit('/fi/feedback')
+    // Call the function that fills in the form fields
     fillInFormFields()
+
+    // Submit the form
     cy.get('#submit-feedback').click()
     cy.injectAxe()
   })
