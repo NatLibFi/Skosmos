23bdcfe4c7cd5f83f3c2a0dcac1c9921c76f3d3f
Joeli Takala <joeli.takala@helsinki.fi>
2024-04-24 16:06:34 +0300

    Added cookie tests. Refactored vocab search bar tests and divided into subsections.

--- tests/cypress/template/vocab-search-bar.cy.js
+++ tests/cypress/template/vocab-search-bar.cy.js
@@ -1,157 +1,183 @@
 describe('Vocab search bar', () => {
-  it('search can be done with a chosen content language', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
 
-    // Select an option from the dropdown
-    cy.get('#search-wrapper select').select('sv');
+  describe('Search Language', () => {
+    it('search can be done with a chosen content language', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
 
-    // Enter a search term
-    cy.get('#search-wrapper input').type('Katt');
+      // Select an option from the dropdown
+      cy.get('#search-wrapper select').select('sv');
 
-    // Click the search button
-    cy.get('#search-button').click();
+      // Enter a search term
+      cy.get('#search-wrapper input').type('Katt');
 
-    //Verify the search page url (search result page tests are elsewhere)
-    cy.url().should('include', 'q=Katt').and('include', 'clang=sv');
+      // Click the search button
+      cy.get('#search-button').click();
 
-  })
+      // Verify the search page url (search result page tests are elsewhere)
+      cy.url().should('include', 'q=Katt').and('include', 'clang=sv');
+    })
 
-  it('search can be done with all languages', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
+    it('search can be done with all languages', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
 
-    // Choose 'all' languages
-    cy.get('#search-wrapper select').select('all');
+      // Choose 'all' languages
+      cy.get('#search-wrapper select').select('all');
 
-    // Enter a search term
-    cy.get('#search-wrapper input').type('Katt');
+      // Enter a search term
+      cy.get('#search-wrapper input').type('Katt');
 
-    // Click the search button
-    cy.get('#search-button').click();
+      // Click the search button
+      cy.get('#search-button').click();
 
-    //Verify the search page url (search result page tests are elsewhere)
-    cy.url().should('include', 'q=Katt').and('include', 'anylang=true');
-  })
+      // Verify the search page url (search result page tests are elsewhere)
+      cy.url().should('include', 'q=Katt').and('include', 'anylang=true');
+    })
+  });
 
-  it('Writing in the text field triggers the autocomplete results list', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
+  describe('Autocomplete', () => {
+    it('Writing in the text field triggers the autocomplete results list', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
 
-    cy.get('#search-field').type('kas'); // perform autocomplete search
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible').children().should('have.length.greaterThan', 2);
-  })
+      cy.get('#search-field').type('kas'); // perform autocomplete search
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible').children().should('have.length.greaterThan', 2);
+    })
 
-  it('No results message is displayed if no results are found', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/en/')
+    it('Special characters can be used in the search', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
 
-    cy.get('#search-field').type('kissa'); // even if the search yields no results, there shoulde a single line in the result list
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible').children().should('have.length.greaterThan', 0);
-    cy.get('#search-autocomplete-results').within(() => {
+      cy.get('#search-field').type('*tus (*');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+
+      cy.get('#search-autocomplete-results').within(() => { // the first result should have text ajoitus (historia)
+        cy.get('li').first().should('contain', 'ajoitus (historia)')
+      })
+    })
+
+    it('No results message is displayed if no results are found', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/en/')
+
+      cy.get('#search-field').type('kissa'); // even if the search yields no results, there shoulde a single line in the result list
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible').children().should('have.length.greaterThan', 0);
+      cy.get('#search-autocomplete-results').within(() => {
         cy.get('li').eq(0).invoke('text').should('contain', 'No results') // the single result should display a no results message
       })
-  })
-
-  it('No results are displayed for autocomplete if there is not at leas two charecters in the search term', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/en/')
-
-    cy.get('#search-field').type('k'); // even if the search yields no results, there shoulde a single line in the result list
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('not.be.visible');
-  })
-
-  it('The autocomplete list should not change due to previous searches completing', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
-
-    cy.get('#search-field').type('ka');
-    cy.wait(300);
-    cy.get('#search-field').type('i');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-    cy.get('#search-autocomplete-results').children().should('have.length', 1)
-    cy.wait(10000); // wait extra 10 seconds to see if the 'ka' search adds results to the list
-    cy.get('#search-autocomplete-results').children().should('have.length', 1)
-  })
-
-  it('Clear button should hide the autocomplete list', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/en/')
-
-    cy.get('#search-field').type('kas');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-
-    cy.get('#clear-button').click()
-    cy.get('#search-autocomplete-results').should('not.be.visible'); // the autocomplete should disappear
-  })
-
-  it('Emptying the text search field hides the autocomplete list', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/en/')
-
-    cy.get('#search-field').type('kis');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-
-    cy.get('#search-field').clear();
-    cy.get('#search-autocomplete-results').should('not.be.visible'); // the autocomplete should disappear
-  })
-
-  it('Clicking outside of the autocomplete list hides the autocomplete list', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/en/')
-
-    cy.get('#search-field').type('kas');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-
-    cy.get('#main-container').click({ force: true }); // using force true to click on elements not considered actionable
-    cy.get('#search-autocomplete-results').should('not.be.visible'); // the autocomplete should disappear
-  })
-
-  it('AltLabel search results should bold the matching parts of altLabel', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
-
-    cy.get('#search-field').type('assyro');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-
-    cy.get('#search-autocomplete-results').within(() => { // the first result should have matching part of text 'assyrologia' appearing in bold
-      cy.get('li').last().find('b').eq(0).should('have.text', 'assyro')
     })
-  })
 
-  it('AltLabel search results should be displayed in italics', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
+    it('No results are displayed for autocomplete if there is not at leas two charecters in the search term', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/en/')
 
-    cy.get('#search-field').type('assyro');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-
-    cy.get('#search-autocomplete-results').within(() => { // the first result should have text 'assyrologia' appearing in italics
-      cy.get('li').last().find('i').eq(0).should('contain.text', 'assyrologia')
+      cy.get('#search-field').type('k'); // even if the search yields no results, there shoulde a single line in the result list
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('not.be.visible');
     })
-  })
 
-  it('Notation search results should bold the matching parts of the notation', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
+    it('The autocomplete list should not change due to previous searches completing', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
 
-    cy.get('#search-field').type('51');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
-
-    cy.get('#search-autocomplete-results').within(() => { // the first result should have text '51' appearing in bold
-      cy.get('li').last().find('b').eq(0).should('have.text', '51')
+      cy.get('#search-field').type('ka');
+      cy.wait(300);
+      cy.get('#search-field').type('i');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+      cy.get('#search-autocomplete-results').children().should('have.length', 1)
+      cy.wait(5000); // wait extra 5 seconds to see if the 'ka' search adds results to the list
+      cy.get('#search-autocomplete-results').children().should('have.length', 1)
     })
-  })
 
-  it('Special characters can be used in the search', () => {
-    // go to YSO vocab front page
-    cy.visit('/yso/fi/')
+    it('Clear button should hide the autocomplete list', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/en/')
 
-    cy.get('#search-field').type('*tus (*');
-    cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+      cy.get('#search-field').type('kas');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
 
-    cy.get('#search-autocomplete-results').within(() => { // the first result should have text ajoitus (historia)
-      cy.get('li').first().should('contain', 'ajoitus (historia)')
+      cy.get('#clear-button').click()
+      cy.get('#search-autocomplete-results').should('not.be.visible'); // the autocomplete should disappear
     })
-  })
+
+    it('Emptying the text search field hides the autocomplete list', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/en/')
+
+      cy.get('#search-field').type('kis');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+
+      cy.get('#search-field').clear();
+      cy.get('#search-autocomplete-results').should('not.be.visible'); // the autocomplete should disappear
+    })
+
+    it('Clicking outside of the autocomplete list hides the autocomplete list', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/en/')
+
+      cy.get('#search-field').type('kas');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+
+      cy.get('#main-container').click({ force: true }); // using force true to click on elements not considered actionable
+      cy.get('#search-autocomplete-results').should('not.be.visible'); // the autocomplete should disappear
+    })
+  });
+
+  describe('Search Result Rendering', () => {
+    it('AltLabel search results should bold the matching parts of altLabel', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
+
+      cy.get('#search-field').type('assyro');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+
+      cy.get('#search-autocomplete-results').within(() => { // the first result should have matching part of text 'assyrologia' appearing in bold
+        cy.get('li').last().find('b').eq(0).should('have.text', 'assyro')
+      })
+    })
+
+    it('AltLabel search results should be displayed in italics', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
+
+      cy.get('#search-field').type('assyro');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+
+      cy.get('#search-autocomplete-results').within(() => { // the first result should have text 'assyrologia' appearing in italics
+        cy.get('li').last().find('i').eq(0).should('contain.text', 'assyrologia')
+      })
+    })
+
+    it('Notation search results should bold the matching parts of the notation', () => {
+      // go to YSO vocab front page
+      cy.visit('/yso/fi/')
+
+      cy.get('#search-field').type('51');
+      cy.get('#search-autocomplete-results', { timeout: 20000 }).should('be.visible'); // the autocomplete should appear
+
+      cy.get('#search-autocomplete-results').within(() => { // the first result should have text '51' appearing in bold
+        cy.get('li').last().find('b').eq(0).should('have.text', '51')
+      })
+    })
+  });
+
+  describe('Cookie Management', () => {
+    it('The search language cookie is set', () => {
+      cy.visit('/yso/fi/');
+
+      // Select an option from the dropdown to set the search language
+      cy.get('#search-wrapper select').select('sv');
+
+      // Test that the cookie has been set correctly
+      cy.getCookie('SKOSMOS_SEARCH_LANG').should('have.property', 'value', 'sv');
+    });
+
+    it('The search language cookie is read', () => {
+      cy.setCookie('SKOSMOS_SEARCH_LANG', 'en');
+      cy.visit('/yso/fi/');
+
+      // Test that the cookie value has been read and used to initialize the language selector
+      cy.get('#search-wrapper select').should('have.value', 'en');
+    });
+  });
 })
