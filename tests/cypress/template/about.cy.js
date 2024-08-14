2f2ccfa18149a4415e4c2f8c250775d3dd947af7
Osma Suominen <osma.suominen@helsinki.fi>
2024-08-14 14:22:45 +0300

    remove redundant twitter:title metadata (og:title should be enough)

--- tests/cypress/template/about.cy.js
+++ tests/cypress/template/about.cy.js
@@ -8,7 +8,6 @@ describe('About page', () => {
     cy.get('title').invoke('text').should('equal', expectedTitle)
     // check that the page has title metadata
     cy.get('head meta[name="title"]').should('have.attr', 'content', expectedTitle);
-    cy.get('head meta[name="twitter:title"]').should('have.attr', 'content', expectedTitle);
     cy.get('head meta[property="og:title"]').should('have.attr', 'content', expectedTitle);
   })
   it('Contains version number information', () => {
