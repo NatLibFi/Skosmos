cdd654e4bebe3bef763cf33095ccfd6367c3ca82
Mika Vaara <mika.vaara@helsinki.fi>
2024-10-03 15:10:32 +0300

    Edited the code slightly to align it more with StandardJS

--- resource/js/vocab-counts.js
+++ resource/js/vocab-counts.js
@@ -1,31 +1,31 @@
-async function loadLocaleMessages() {
-  const locales = ['en', 'fi'];
-  const messages = {};
+async function loadLocaleMessages () {
+  const locales = ['en', 'fi']
+  const messages = {}
 
   for (const locale of locales) {
-    const response = await fetch(`http://localhost/skosmos/resource/translations/messages.${locale}.json`);
-    const data = await response.json();
-    console.log(data);
-    messages[locale] = data;
+    const response = await fetch(`http://localhost/skosmos/resource/translations/messages.${locale}.json`)
+    const data = await response.json()
+    console.log(data)
+    messages[locale] = data
   }
 
-  return messages;
+  return messages
 }
 
-async function createI18nInstance() {
-  const messages = await loadLocaleMessages();
+async function createI18nInstance () {
+  const messages = await loadLocaleMessages()
 
   const i18n = VueI18n.createI18n({
     locale: window.SKOSMOS.lang || 'en', // oletuskieli
     fallbackLocale: 'en', // Fallback
-    messages,
-  });
+    messages
+  })
 
-  return i18n;
+  return i18n
 }
 
-(async function() {
-  const i18n = await createI18nInstance();
+(async function () {
+  const i18n = await createI18nInstance()
 
   const resourceCountsApp = Vue.createApp({
     data () {
@@ -42,14 +42,14 @@ async function createI18nInstance() {
     },
     mounted () {
       fetch('rest/v1/' + window.SKOSMOS.vocab + '/vocabularyStatistics?lang=' + window.SKOSMOS.lang)
-          .then(data => {
-            return data.json()
-          })
-          .then(data => {
-            this.concepts = data.concepts
-            this.subTypes = data.subTypes
-            this.conceptGroups = data.conceptGroups
-          })
+        .then(data => {
+          return data.json()
+        })
+        .then(data => {
+          this.concepts = data.concepts
+          this.subTypes = data.subTypes
+          this.conceptGroups = data.conceptGroups
+        })
     },
     template: `
       <h3 class="fw-bold py-3">{{ $t('Resource counts by type') }}</h3>
@@ -59,7 +59,7 @@ async function createI18nInstance() {
         <template v-if="hasCounts">
           <resource-counts :concepts="concepts" :subTypes="subTypes" :conceptGroups="conceptGroups"></resource-counts>
         </template>
-        <template v-else >
+        <template v-else>
           <i class="fa-solid fa-spinner fa-spin-pulse"></i>
         </template>
         </tbody>
@@ -74,7 +74,7 @@ async function createI18nInstance() {
         <td>{{ concepts.label }}</td>
         <td>{{ concepts.count }}</td>
       </tr>
-      <tr v-for="st in subTypes">
+      <tr v-for="st in subTypes" :key="st.label">
         <td>* {{ st.label }}</td>
         <td>{{ st.count }}</td>
       </tr>
@@ -89,6 +89,6 @@ async function createI18nInstance() {
     `
   })
 
-  resourceCountsApp.use(i18n);
-  resourceCountsApp.mount('#resource-counts');
-})();
+  resourceCountsApp.use(i18n)
+  resourceCountsApp.mount('#resource-counts')
+})()
