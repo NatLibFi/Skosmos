1b519bccde07f4fa7d4e87ab212c11e1b08c3ac9
Joeli Takala <joeli.takala@helsinki.fi>
2024-10-03 13:21:28 +0300

    Added tests for the contents of the search bar language menu
    Translations can be maintained with the Symfony-Lokalise combination (pull, push, extract)  but not yet by extracting from Vue code (#1648)
    
    At first, there was some uncertainty about whether the system was actually updating the data correctly (deletion in Lokalise -> deletion locally as well), but it seems that it worked correctly after all. Although the feature branch can be merged, further testing should still be done just to be sure.
    
    Add cypress tests for alphabetical index + add new test vocab
    
    Add cypress test for alt labels
    
    Add cypress files for alphabetical index and hierarchy
    
    Move alphabetical index cypress tests from vocab-home.cy.js + add checks for spinners
    
    Change test vocab name
    
    Fix uriSpace
    
    Fix uri space in test vocab

--- tests/cypress/template/vocab-search-bar.cy.js
+++ tests/cypress/template/vocab-search-bar.cy.js
@@ -53,6 +53,23 @@ describe('Vocab search bar', () => {
       cy.url().should('include', 'clang=sv');
     })
 
+    it('available search languages are the ones described in the vocabulary config', () => {
+      cy.visit('/yso/en/') // go to the YSO home page in English language
+
+      // check that the vocabulary languages can be found in the search bar language dropdown menu
+      cy.window().then((win) => {
+        cy.get('#language-list .dropdown-item').then($elements => {
+          const actualLanguages = $elements.map((index, el) => Cypress.$(el).attr('value')).get();
+
+          // The expected languages should be those found in vocabulary configuration with an additional 'all' language
+          const expectedLanguages = [...win.SKOSMOS.languages, 'all'];
+
+          // The two language lists should be of equal length and all of the expected languages can be found
+          expect(expectedLanguages).to.have.lengthOf(actualLanguages.length);
+          expectedLanguages.forEach(lang => { expect(actualLanguages).to.include(lang); });
+        })
+      })
+    })
   });
 
   describe('Autocomplete', () => {
