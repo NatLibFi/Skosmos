7dcec2226899703330be27f086eb37d2edc4824f
Joeli Takala <joeli.takala@helsinki.fi>
2024-12-11 13:39:37 +0200

    Fixed JS style

--- resource/js/navbar-ui-languages.js
+++ resource/js/navbar-ui-languages.js
@@ -1,4 +1,4 @@
-function setLangCookie(lang) {
+function setLangCookie (lang) {
   // The cookie path should be relative if the baseHref is known
   let cookiePath = '/'
   if (window.SKOSMOS.baseHref && window.SKOSMOS.baseHref.replace(window.origin, '')) {
@@ -6,11 +6,11 @@ function setLangCookie(lang) {
   }
   document.cookie = `SKOSMOS_LANGUAGE=${lang};path=${cookiePath}`
 }
-function addLanguageEventListeners() {
-  const languageLinks = document.querySelectorAll('.nav-item.language a');
-  languageLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      const langValue = this.getAttribute('id').substring(9) //strip the 'language-' part of the id
+function addLanguageEventListeners () {
+  const languageLinks = document.querySelectorAll('.nav-item.language a')
+  languageLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      const langValue = this.getAttribute('id').substring(9) // strip the 'language-' part of the id
       setLangCookie(langValue)
     })
   })
