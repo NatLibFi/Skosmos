c0c4432f766adfd315837e39c8168992e6987965
Joeli Takala <joeli.takala@helsinki.fi>
2024-12-11 13:27:54 +0200

    Cypress tests

---
+++ tests/cypress/template/navbar-languages.cy.js
@@ -0,0 +1,12 @@
+describe('Navbar languages', () => {
+
+  it('Sets and reads SKOSMOS_LANGUAGE cookie correctly', () => {
+    cy.visit('/')
+
+    cy.get('.nav-item.language a').first().click()
+    cy.getCookie('SKOSMOS_LANGUAGE').should('exist')
+    cy.getCookie('SKOSMOS_LANGUAGE').should('have.property', 'value', 'fi')
+
+    cy.url().should('include', '/fi/')
+  })
+})
