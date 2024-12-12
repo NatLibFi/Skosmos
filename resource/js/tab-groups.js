950cb5fd052ef234ccdc4c482845ccdf21057bf1
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2024-12-12 14:03:15 +0200

    Add UI elements for groups view

--- resource/js/tab-groups.js
+++ resource/js/tab-groups.js
@@ -6,11 +6,16 @@ const tabGroupsApp = Vue.createApp({
     return {
       groups: [],
       selectedGroup: '',
+      loadingGroups: true,
+      loadingChildGroups: [],
+      listStyle: {}
     }
   },
   provide () {
     return {
-      
+      partialPageLoad,
+      getConceptURL,
+      showNotation: window.SKOSMOS.showNotation
     }
   },
   mounted () {
@@ -30,11 +35,16 @@ const tabGroupsApp = Vue.createApp({
       }
     },
     loadGroups () {
+      this.loadingGroups = true
       fetch('rest/v1/' + window.SKOSMOS.vocab + '/groups/?lang=' + window.SKOSMOS.content_lang)
         .then(data => {
           return data.json()
         })
         .then(data => {
+          console.log('groups data', data)
+
+          this.groups = []
+
           const groups = data.groups
           const result = []
         
@@ -75,6 +85,7 @@ const tabGroupsApp = Vue.createApp({
                 return data.json()
               })
               .then(data => {
+                console.log('members data', data)
                 // Filter out existing groups from members list and add the correct properties
                 const members = data.members
                   .filter(m => !uriMap.has(m.uri))
@@ -87,11 +98,13 @@ const tabGroupsApp = Vue.createApp({
                 this.setIsOpenAndAddMembers(result, this.selectedGroup, members)
 
                 this.groups = result
+                this.loadingGroups = false
                 console.log("groups", this.groups)
               })
           } else {
-            // If we are on vocab home page, just set groups
+            // If we are on vocab home page, simply set groups
             this.groups = result
+            this.loadingGroups = false
             console.log("groups", this.groups)
           }
         })
@@ -122,10 +135,28 @@ const tabGroupsApp = Vue.createApp({
       for (const root of tree) {
         findAndSet(root)
       }
+    },
+    setListStyle () {
+      // TODO: set list style when mounting component and resizing window
+    },
+    loadChildGroups () {
+
     }
   },
   template: `
-    <div v-click-tab-groups="handleClickGroupsEvent">
+    <div v-click-tab-groups="handleClickGroupsEvent" v-resize-window="setListStyle">
+      <div id="groups-list" class="sidebar-list p-0" :style="listStyle">
+        <ul v-if="!loadingGroups" class="list-group">
+          <tab-groups-wrapper
+            :groups="groups"
+            :selectedGroup="selectedGroup"
+            :loadingChildGroups="loadingChildGroups"
+            @load-child-groups="loadChildGroups($event)"
+            @select-group="selectedGroup = $event"
+          ></tab-groups-wrapper>
+        </ul>
+        <i v-else class="fa-solid fa-spinner fa-spin-pulse"></i>
+      </div>
     </div>
   `
 })
@@ -141,6 +172,111 @@ tabGroupsApp.directive('click-tab-groups', {
   unmounted: el => {
     document.querySelector('#groups').removeEventListener('click', el.clickTabEvent)
   }
+})
+
+/* Custom directive used to add an event listener on resizing the window */
+tabGroupsApp.directive('resize-window', {
+  beforeMount: (el, binding) => {
+    el.resizeWindowEvent = event => {
+      binding.value() // calling the method given as the attribute value (setListStyle)
+    }
+    window.addEventListener('resize', el.resizeWindowEvent) // registering an event listener on resizing the window
+  },
+  unmounted: el => {
+    window.removeEventListener('resize', el.resizeWindowEvent)
+  }
+})
+
+tabGroupsApp.component('tab-groups-wrapper', {
+  props: ['groups', 'selectedGroup', 'loadingChildGroups'],
+  emits: ['loadChildGroups', 'selectGroup'],
+  mounted () {
+  },
+  methods: {
+    loadChildGroups (group) {
+      this.$emit('loadChildGroups', group)
+    },
+    selectGroup (group) {
+      this.$emit('selectGroup', group)
+    }
+  },
+  template: `
+    <template v-for="(g, i) in groups" >
+      <tab-groups
+        :group="g"
+        :selectedGroup="selectedGroup"
+        :isTopGroup="true"
+        :isLast="i == groups.length - 1"
+        :loadingChildGroups="loadingChildGroups"
+        @load-child-groups="loadChildGroups($event)"
+        @select-group="selectGroup($event)"
+      ></tab-groups>
+    </template>
+  `
+})
+
+tabGroupsApp.component('tab-groups', {
+  props: ['group', 'selectedGroup', 'isTopGroup', 'isLast', 'loadingChildGroups'],
+  emits: ['loadChildGroups', 'selectGroup'],
+  inject: ['partialPageLoad', 'getConceptURL', 'showNotation'],
+  methods: {
+    handleClickOpenEvent (group) {
+      group.isOpen = !group.isOpen
+      this.$emit('loadChildGroups', group)
+    },
+    handleClickGroupEvent (event, group) {
+      group.isOpen = true
+      this.$emit('loadChildGroups', group)
+      this.$emit('selectGroup', group.uri)
+      this.partialPageLoad(event, this.getConceptURL(group.uri))
+    },
+    loadChildGroupsRecursive (group) {
+      this.$emit('loadChildGroups', group)
+    },
+    selectGroupRecursive (group) {
+      this.$emit('selectGroup', group)
+    }
+  },
+  template: `
+    <li class="list-group-item p-0" :class="{ 'top-concept': isTopGroup }">
+      <button type="button" class="hierarchy-button btn btn-primary" aria-label="Open"
+        :class="{ 'open': group.isOpen }"
+        v-if="group.hasMembers"
+        @click="handleClickOpenEvent(group)"
+      >
+        <template v-if="loadingChildGroups.includes(group)">
+          <i class="fa-solid fa-spinner fa-spin-pulse"></i>
+        </template>
+        <template v-else>
+          <img v-if="group.isOpen" alt="" src="resource/pics/black-lower-right-triangle.png">
+          <img v-else alt="" src="resource/pics/lower-right-triangle.png">
+        </template>
+      </button>
+      <span class="concept-label" :class="{ 'last': isLast }">
+        <a :class="{ 'selected': selectedGroup === group.uri }"
+          :href="getConceptURL(group.uri)"
+          @click="handleClickGroupEvent($event, group)"
+          aria-label="Go to the concept page"
+        >
+          <span v-if="showNotation && group.notation" class="concept-notation">{{ group.notation }} </span>
+          {{ group.prefLabel }}
+        </a>
+      </span>
+      <ul class="list-group ps-3" v-if="group.childGroups.length !== 0 && group.isOpen">
+        <template v-for="(g, i) in group.childGroups">
+          <tab-groups
+            :group="g"
+            :selectedGroup="selectedGroup"
+            :isTopGroup="false"
+            :isLast="i == group.childGroups.length - 1"
+            :loadingChildGroups="loadingChildGroups"
+            @load-child-groups="loadChildGroupsRecursive($event)"
+            @select-group="selectGroupRecursive($event)"
+          ></tab-groups>
+        </template>
+      </ul>
+    </li>
+  `
 })
 
 tabGroupsApp.mount('#tab-groups')
