bca6249656dc8937a6fdb163e20567f249900256
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2025-01-29 14:42:02 +0200

    Use default word order for Sami language date

--- resource/js/tab-changes.js
+++ resource/js/tab-changes.js
@@ -55,11 +55,6 @@ function startChangesApp () {
             for (const concept of data.changeList) {
               const date = new Date(concept.date)
               let key = date.toLocaleString(window.SKOSMOS.lang, { month: 'long', year: 'numeric' })
-
-              // Reverse Northern Sami word order (year month -> month year)
-              if (window.SKOSMOS.lang === 'se') {
-                key = key.split(' ').reverse().join(' ')
-              }
               // Capitalize month name
               key = key.charAt(0).toUpperCase() + key.slice(1)
 
