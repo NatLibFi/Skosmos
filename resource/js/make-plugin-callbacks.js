f007cbab8e9329e1e8b3ceab3261942c56e2fdff
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2025-05-21 08:51:14 +0300

    Add plugins to scrpts.inc and add function to make plugin callbacks

---
+++ resource/js/make-plugin-callbacks.js
@@ -0,0 +1,27 @@
+const makeCallbacks = () => {
+  const newPage = window.SKOSMOS.page || 'page'
+  const newUri = window.SKOSMOS.uri
+  const newPrefs = window.SKOSMOS.prefLabels
+  const ldJsonScript = document.querySelector('script[type="application/ld+json"]')
+  const embeddedJsonLd  = ldJsonScript ? JSON.parse(ldJsonScript.innerHTML) : {}
+
+  const params = {'uri': newUri, 'prefLabels': newPrefs, 'page': newPage, 'json-ld': embeddedJsonLd}
+
+  if (window.pluginCallbacks) {
+    for (let i in window.pluginCallbacks) {
+      const fname = window.pluginCallbacks[i]
+      const callback = window[fname]
+      if (typeof callback === 'function') {
+        callback(params)
+      }
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  makeCallbacks()
+})
+
+document.addEventListener('loadConceptPage', () => {
+  makeCallbacks()
+})
