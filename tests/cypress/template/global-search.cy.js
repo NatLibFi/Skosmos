651d6109c456f9e0b2842741d297be0d72c6775b
Joeli Takala <joeli.takala@helsinki.fi>
2025-11-04 16:43:19 +0200

    Amend existing tests to reflect changes in the global search bar

--- tests/cypress/template/global-search.cy.js
+++ tests/cypress/template/global-search.cy.js
@@ -1,7 +1,7 @@
 describe('Global search page', () => {
   const term = 'bass';
   it('Contains title and title metadata', () => {
-      cy.visit(`/en/search?clang=en&q=${term}`)
+      cy.visit(`/en/search?clang=en&query=${term}`)
 
       const expectedTitle = "'bass' - Skosmos being tested"
       // check that the page has a HTML title
@@ -11,16 +11,16 @@ describe('Global search page', () => {
       cy.get('head meta[property="og:title"]').should('have.attr', 'content', expectedTitle);
   })
   it('Contains site name metadata', () => {
-      cy.visit(`/en/search?clang=en&q=${term}`)
+      cy.visit(`/en/search?clang=en&query=${term}`)
 
       const expectedSiteName = 'Skosmos being tested'
       // check that the page has site name metadata
       cy.get('head meta[property="og:site_name"]').should('have.attr', 'content', expectedSiteName);
   })
   it('Contains canonical URL metadata', () => {
-      cy.visit(`/en/search?clang=en&q=${term}`)
+      cy.visit(`/en/search?clang=en&query=${term}`)
 
-    const expectedUrl = Cypress.config('baseUrl') + `en/search?clang=en&q=${term}`
+    const expectedUrl = Cypress.config('baseUrl') + `en/search?clang=en&query=${term}`
     // check that the page has canonical URL metadata
     cy.get('link[rel="canonical"]').should('have.attr', 'href', expectedUrl);
     cy.get('head meta[property="og:url"]').should('have.attr', 'content', expectedUrl);
@@ -28,7 +28,7 @@ describe('Global search page', () => {
   it('Contains correct amount of search results ', () => {
       const count = 1;
       const searchCountTitle = `${count} results for '${term}'`;
-      cy.visit(`/en/search?clang=en&q=${term}`)
+      cy.visit(`/en/search?clang=en&query=${term}`)
 
       //Check that the search count is correct
       cy.get('#search-results > h1').invoke('text').should('contain', searchCountTitle);
@@ -37,7 +37,7 @@ describe('Global search page', () => {
       cy.get('div.search-result').should('have.length', count)
   })
   it('Search results contains correct info', () => {
-      cy.visit(`/en/search?clang=en&q=${term}`)
+      cy.visit(`/en/search?clang=en&query=${term}`)
 
       //Check that there is a search result that contains a type icon
       cy.get('div.search-result > ul > li > span > i.property-hover.fa-solid.fa-arrows-to-circle')
