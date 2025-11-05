56d5eded605e18ac30160f3524f6f0d598967b78
Osma Suominen <osma.suominen@helsinki.fi>
2025-11-05 16:16:23 +0200

    initial implementation of truncated property value lists

---
+++ resource/js/truncate-property-values.js
@@ -0,0 +1,33 @@
+function truncatePropertyValues () {
+  const maxValues = 15 // hide extras if there are more values than this
+  const propVals = document.querySelectorAll('.property-value ul')
+  propVals.forEach((propVal) => { // one ul within div.property-value
+    const listItems = propVal.children // the li's inside the ul
+
+    if (propVal.querySelector('a.property-value-show') === null && listItems.length > maxValues) {
+      // hide the items after the first maxValues
+      for (let i = maxValues; i < listItems.length; ++i) {
+        listItems[i].classList.add('property-value-hidden')
+      }
+      // add a link for showing all items
+      const showItem = document.createElement('li')
+      const showLink = document.createElement('a')
+      showLink.href = '#'
+      showLink.textContent = 'Show all'
+      showLink.classList.add('property-value-show')
+
+      showLink.addEventListener('click', function (event) {
+        event.preventDefault()
+        propVal.classList.add('property-value-expand')
+        event.target.closest('li').remove()
+      })
+
+      showItem.appendChild(showLink)
+      propVal.appendChild(showItem)
+    }
+  })
+}
+
+// Event listeners when page is initially loaded and after partial page load
+document.addEventListener('DOMContentLoaded', truncatePropertyValues)
+document.addEventListener('loadConceptPage', truncatePropertyValues)
